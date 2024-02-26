d1d7292ea46120ed26e0d83e47b5ee04c098fe2d
bhavabhuthi <bhavabhuthi@swecha.net>
2024-02-26 12:35:00 +0530

    Add layout for home and other pages in user group

---
+++ app/[locale]/(user)/layout.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import React from 'react';
+import { notFound, usePathname } from 'next/navigation';
+
+import { MainNav } from '@/components/main-nav';
+
+interface UserLayoutProps {
+  children?: React.ReactNode;
+}
+
+export default async function Layout({ children }: UserLayoutProps) {
+  const routerPath = usePathname();
+  const user = true; // await getCurrentUser()
+  const regEx = new RegExp(/\/[a-z]*\/[a-z]*/);
+
+  if (!user) {
+    return notFound();
+  }
+
+  return (
+    <div className="flex h-full grow flex-col">
+      <header className="relative z-2 bg-surfaceDefault px-4 py-3 shadow-elementTopNav">
+        <MainNav hideSearch={!regEx.test(routerPath)} />
+      </header>
+      <>{children}</>
+    </div>
+  );
+}
