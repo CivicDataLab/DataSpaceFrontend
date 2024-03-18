ac50b84ab924815f457f4426ba0aba8dc2085fcd
sanjaypinna <sanjaypinna265@gmail.com>
2024-03-18 13:34:14 +0530

    add a filter component

---
+++ app/[locale]/(user)/datasets/components/FIlter/Filter.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Box, CheckboxGroup, Text } from 'opub-ui';
+import { twMerge } from 'tailwind-merge';
+
+import styles from './filter.module.scss';
+
+const Filter = () => {
+  const filtersData = [
+    {
+      title: 'Organization',
+      Options: [
+        {
+          label: 'Option1',
+          value: 'Option1',
+        },
+        {
+          label: 'Option2',
+          value: 'Option2',
+        },
+        {
+          label: 'Option3',
+          value: 'Option3',
+        },
+      ],
+    },
+    {
+      title: 'Sector',
+      Options: [
+        {
+          label: 'Option1',
+          value: 'Option1',
+        },
+        {
+          label: 'Option2',
+          value: 'Option2',
+        },
+        {
+          label: 'Option3',
+          value: 'Option3',
+        },
+      ],
+    },
+    {
+      title: 'Category',
+      Options: [
+        {
+          label: 'Option1',
+          value: 'Option1',
+        },
+        {
+          label: 'Option2',
+          value: 'Option2',
+        },
+        {
+          label: 'Option3',
+          value: 'Option3',
+        },
+      ],
+    },
+  ];
+
+  return (
+    <Box>
+      {filtersData.map((item, index) => (
+        <div key={index}>
+          <div className={twMerge(styles.filter, 'px-3 py-1')}>
+            <Text variant="headingSm" color="onBgDefault">
+              {item.title}
+            </Text>
+          </div>
+          <div className={twMerge(styles.options, 'p-3')}>
+            <CheckboxGroup
+              name="checkbox"
+              options={item.Options.map((Option) => ({
+                label: Option.label,
+                value: Option.value,
+              }))}
+              title={undefined}
+            />
+          </div>
+        </div>
+      ))}
+    </Box>
+  );
+};
+
+export default Filter;
