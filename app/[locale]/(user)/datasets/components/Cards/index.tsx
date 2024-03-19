28e29aa8b3ea2b3e75ea79babe86a51e93b973df
sanjaypinna <sanjaypinna265@gmail.com>
2024-03-19 12:20:48 +0530

    fix alignment issue

--- app/[locale]/(user)/datasets/components/Cards/index.tsx
+++ app/[locale]/(user)/datasets/components/Cards/index.tsx
@@ -38,15 +38,15 @@ const Cards = ({ data }: { data: Dataset }) => {
         </div>
         <div className="mt-5 flex flex-wrap  gap-4 lg:gap-8">
           <div className="flex flex-col gap-3.5 md:w-1/2 lg:w-1/2">
-            <div className="align-center flex flex-wrap">
+            <div className="flex flex-wrap items-center">
               <Text fontWeight="bold">Frequency of Update&nbsp;:</Text>
               <Text>&nbsp;{metadata.update}</Text>
             </div>
-            <div className="align-center flex flex-wrap">
+            <div className="flex flex-wrap items-center">
               <Text fontWeight="bold">Categories&nbsp;:</Text>
               <Text>&nbsp;{metadata.category}</Text>
             </div>
-            <div className="align-center flex flex-wrap">
+            <div className="flex flex-wrap items-center">
               <Text fontWeight="bold">Tags&nbsp;:&nbsp;</Text>
               <div className="flex gap-2">
                 {metadata.tags.map((item, index) => (
@@ -56,7 +56,7 @@ const Cards = ({ data }: { data: Dataset }) => {
             </div>
           </div>
           <div className="flex flex-col gap-3.5 md:w-1/3 lg:w-1/3">
-            <div className="align-center flex flex-wrap">
+            <div className="flex flex-wrap items-center">
               <Text fontWeight="bold">Formats&nbsp;:&nbsp;</Text>
               {/* <Text>&nbsp;Monthly</Text> */}
               <div className="flex gap-2">
@@ -65,7 +65,7 @@ const Cards = ({ data }: { data: Dataset }) => {
                 ))}
               </div>
             </div>
-            <div className="align-center flex flex-wrap">
+            <div className="flex flex-wrap items-center">
               <Text fontWeight="bold">Access Models&nbsp;:</Text>
               <Text>&nbsp;{metadata.accessModelsCount} in total</Text>&nbsp;
               <div className="flex gap-2">
