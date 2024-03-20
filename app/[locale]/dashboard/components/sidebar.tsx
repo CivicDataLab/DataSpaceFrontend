77927a411bd726a3c440bbf41c354ac7d64a9555
sanjaypinna <sanjaypinna265@gmail.com>
2024-03-20 18:23:31 +0530

    add Sidebar component

---
+++ app/[locale]/dashboard/components/sidebar.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { Button, Icon, Sheet, Text } from 'opub-ui';
+
+import { Icons } from '@/components/icons';
+
+const Sidebar = () => {
+  const [open, setOpen] = useState(false);
+
+  const data = [
+    {
+      label: 'Home',
+      href: '/',
+    },
+    {
+      label: 'Dataset Listing',
+      href: '/datasets',
+    },
+  ];
+  return (
+    <>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <Sheet.Trigger>
+          <Button kind="tertiary" onClick={() => setOpen(true)}>
+            <Icon source={Icons.menu} />
+          </Button>
+        </Sheet.Trigger>
+        <Sheet.Content className=" w-3/5  p-4">
+          <div className="mb-2 flex justify-between">
+            <div>
+              <Text variant="headingMd" as="h1" className="px-1">
+                Data Exchange
+              </Text>
+            </div>
+            <Button onClick={() => setOpen(false)} kind="tertiary">
+              <Icon source={Icons.cross} size={24} color="default" />
+            </Button>
+          </div>
+          {data.map((item, index) => (
+            <div key={index} className="mb-1 px-1 py-2 ">
+              <Link href={item.href} onClick={() => setOpen(false)}>
+                <Text variant="headingSm" as="h1" color={'highlight'}>
+                  {item.label}
+                </Text>
+              </Link>
+            </div>
+          ))}
+        </Sheet.Content>
+      </Sheet>
+    </>
+  );
+};
+
+export default Sidebar;
