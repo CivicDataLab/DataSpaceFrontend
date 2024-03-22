6c0551cb2de190763cd166e9b8b587d5710eb8db
sanjaypinna <sanjaypinna265@gmail.com>
2024-03-22 13:16:26 +0530

    redesign ui and all necessary components

--- app/[locale]/(user)/datasets/[datasetIdentifier]/page.tsx
+++ app/[locale]/(user)/datasets/[datasetIdentifier]/page.tsx
@@ -1,14 +1,94 @@
-import { InProgress } from '@/components/in-progress';
+'use client';
 
-export default function DatasetDetailsPage({
-  params,
-}: {
-  params: { datasetIdentifier: string };
-}) {
+import React, { useState } from 'react';
+import { Button, Icon, Tab, TabList, TabPanel, Tabs, Tray } from 'opub-ui';
+
+import BreadCrumbs from '@/components/BreadCrumbs';
+import { Icons } from '@/components/icons';
+import { data } from '../data';
+import AccessModels from './components/AccessModels';
+import Metadata from './components/Metadata';
+import PrimaryData from './components/PrimaryData';
+import Resources from './components/Resources';
+
+const DatasetDetailsPage = () => {
+  const DatasetInfo = data[1];
+  const [open, setOpen] = useState(false);
+
+  const TabsList = [
+    {
+      label: 'Resources',
+      value: 'resources',
+      component: <Resources data={data[1].resources} />,
+    },
+    {
+      label: 'Access Models',
+      value: 'accessmodels',
+      component: <AccessModels data={data[1].accessModels} />,
+    },
+  ];
   return (
-    <main className="flex h-full w-full flex-col items-center justify-center gap-2">
-      <text>{params.datasetIdentifier}</text>
-      <InProgress />
+    <main className="mx-5 flex gap-8">
+      <div
+        className="w-full py-5 lg:w-9/12"
+        style={{ backgroundColor: 'var( --base-pure-white)' }}
+      >
+        {' '}
+        <BreadCrumbs
+          data={[
+            { href: '/', label: 'Home' },
+            { href: '/datasets', label: 'Dataset Listing' },
+            { href: '#', label: 'Dataset Details' },
+          ]}
+        />
+        <div className="mx-6 block flex flex-col gap-5 py-6 md:mx-14 lg:mx-14">
+          <PrimaryData data={DatasetInfo} />
+          <div
+            className="sm:block md:block lg:hidden"
+            title="About the Dataset"
+          >
+            <Tray
+              size="narrow"
+              open={open}
+              onOpenChange={setOpen}
+              trigger={
+                <>
+                  <Button
+                    kind="tertiary"
+                    className="lg:hidden"
+                    onClick={(e) => setOpen(true)}
+                  >
+                    <Icon source={Icons.info} size={24} color="default" />
+                  </Button>
+                </>
+              }
+            >
+              <Metadata data={DatasetInfo} setOpen={setOpen} />
+            </Tray>
+          </div>
+          <div className="mt-5">
+            <Tabs defaultValue="resources">
+              <TabList fitted>
+                {TabsList.map((item, index) => (
+                  <Tab value={item.value} key={index}>
+                    {item.label}
+                  </Tab>
+                ))}
+              </TabList>
+              {TabsList.map((item, index) => (
+                <TabPanel value={item.value} key={index}>
+                  {item.component}
+                </TabPanel>
+              ))}
+            </Tabs>
+          </div>
+        </div>
+      </div>
+      <div className="hidden pt-16 lg:block">
+        <Metadata data={DatasetInfo} />
+      </div>
     </main>
   );
-}
+};
+
+export default DatasetDetailsPage;
