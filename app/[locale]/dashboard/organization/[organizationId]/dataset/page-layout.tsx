612c15809a7213947b1e34abce31c7f6c237b5f7
bhavabhuthi <bhavabhuthi@swecha.net>
2024-04-15 16:45:13 +0530

    feat: add mutation integration to the action bar in layout

--- app/[locale]/dashboard/organization/[organizationId]/dataset/page-layout.tsx
+++ app/[locale]/dashboard/organization/[organizationId]/dataset/page-layout.tsx
@@ -1,61 +1,60 @@
 'use client';
 
+import { useParams, useRouter } from 'next/navigation';
 import { graphql } from '@/gql';
+import { AddDatasetPayload } from '@/gql/generated/graphql';
 import { usePRouter } from '@/hooks/use-prouter';
 import { CreateDataset } from '@/types';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { Divider } from 'opub-ui';
 
 import { GraphQL } from '@/lib/api';
 import { ActionBar } from './components/action-bar';
 import { Content } from './components/content';
 
-// const allDatasetsQueryDoc = graphql(`
-//   query allDatasetsQuery {
-//     all_datasets {
-//       id
-//       title
-//       description
-//     }
-//   }
-// `);
-
 const createDatasetMutationDoc: any = graphql(`
-  mutation {
+  mutation GenerateDatasetName {
     addDataset {
-      id
-      title
+      __typename
+      ... on TypeDataset {
+        id
+        created
+      }
+      ... on OperationInfo {
+        messages {
+          kind
+          message
+        }
+      }
     }
   }
 `);
 
+
 export const Page = () => {
-  // const { data } = useQuery(['all_datasets'], () =>
-  //   GraphQL(allDatasetsQueryDoc)
-  // );
+  const params = useParams();
+  const router = useRouter();
 
   const queryClient = useQueryClient();
+
   const { mutate, isLoading } = useMutation(
     () => GraphQL(createDatasetMutationDoc, []),
     {
-      onSuccess: (data) => {
-        console.log(data);
+      onSuccess: (data: any) => {
+        queryClient.invalidateQueries({
+          queryKey: [`create_dataset_${'52'}`],
+        });
 
-        // queryClient.invalidateQueries({
-        //   queryKey: [`create_dataset_${'52'}`],
-        // });
-
-        // router.push(
-        //   `/dashboard/dataset/${data?.addDataset?.id}/edit/metadata`
-        // );
+        router.push(
+          `/dashboard/organization/${params.organizationId}/dataset/${data?.addDataset?.id}/edit/distribution`
+        );
       },
       onError: (err: any) => {
-        console.log(err);
+        console.log('Error ::: ', err);
       },
     }
   );
 
-  const router = usePRouter();
   // React.useEffect(() => {
   //   router.prefetch('/dashboard/dataset/new');
   // }, []);
@@ -64,16 +63,11 @@ export const Page = () => {
     <>
       <ActionBar
         title="My Datasets"
-        preFetch="/dashboard/dataset/new"
         primaryAction={{
           content: 'Add New Dataset',
-          // TODO: Replace with dataset creation query
           onAction: () => {
-            console.log('Clicked the Primary Action');
-
             mutate();
           },
-          // router.push('/dashboard/dataset/2345/edit/distribution'),
         }}
         isLoading={isLoading}
       />
