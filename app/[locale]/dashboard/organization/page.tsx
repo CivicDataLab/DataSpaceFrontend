3018a0203ad7297356654896edf828192a394a55
sanjaypinna <sanjaypinna265@gmail.com>
2024-04-16 13:09:35 +0530

    remove async function and add breadcrumbs

--- app/[locale]/dashboard/organization/page.tsx
+++ app/[locale]/dashboard/organization/page.tsx
@@ -5,11 +5,12 @@ import { usePathname } from 'next/navigation';
 import { Button, IconButton } from 'opub-ui';
 
 import { cn } from '@/lib/utils';
+import BreadCrumbs from '@/components/BreadCrumbs';
 import { LinkButton } from '@/components/Link';
 import { DashboardHeader } from '../components/dashboard-header';
 import styles from './../components/styles.module.scss';
 
-export default async function Page({ params }: { params: { id: string } }) {
+const Page = ({ params }: { params: { id: string } }) => {
   const pathname = usePathname();
 
   const organizationsList = [
@@ -24,48 +25,68 @@ export default async function Page({ params }: { params: { id: string } }) {
   ];
 
   return (
-    <div className="flex w-full flex-col">
-      <DashboardHeader currentPath={pathname} />
-
-      <div className={cn(styles.Main)}>
-        <div className="flex flex-wrap gap-24 p-16">
-          {organizationsList.map((orgItem) => (
-            <div
-              id={orgItem.slug}
-              className="flex flex-col items-center gap-2 text-center"
-              key={orgItem.slug}
-            >
-              {/* <Image src={`${NEXT_PUBLIC_BACKEND_URL}`} /> */}
-
+    <div className=" bg-surfaceDefault">
+      <div className="bg-baseGraySlateAlpha1 px-5 py-3 ">
+        <BreadCrumbs
+          data={[
+            { href: '/', label: 'Home' },
+            {
+              href: '/dashboard/user/datasets',
+              label: 'User Dashboard',
+            },
+            {
+              href: '#',
+              label: pathname.includes('organization')
+                ? 'My Organizations'
+                : 'My Personal Datasets',
+            },
+          ]}
+        />
+      </div>
+      <div className="m-auto flex w-11/12 flex-col">
+        <DashboardHeader currentPath={pathname} />
+        <div className={cn(styles.Main)}>
+          <div className="flex flex-wrap gap-24 p-16">
+            {organizationsList.map((orgItem) => (
               <div
-                className="border-var(--border-radius-5) h-36 w-36 rounded-3 opacity-50"
-                style={{ backgroundColor: 'var(--base-amber-solid-7)' }}
+                id={orgItem.slug}
+                className="flex flex-col items-center gap-2 text-center"
+                key={orgItem.slug}
               >
-                <IconButton
-                  size="large"
-                  icon={'userSettings'}
-                  withTooltip
-                  tooltipSide="right"
-                  onClick={() => console.log(orgItem)}
+                {/* <Image src={`${NEXT_PUBLIC_BACKEND_URL}`} /> */}
+
+                <div
+                  className="border-var(--border-radius-5) h-36 w-36 rounded-3 opacity-50"
+                  style={{ backgroundColor: 'var(--base-amber-solid-7)' }}
                 >
-                  Organization
-                </IconButton>
+                  <IconButton
+                    size="large"
+                    icon={'userSettings'}
+                    withTooltip
+                    tooltipSide="right"
+                    onClick={() => console.log(orgItem)}
+                  >
+                    Organization
+                  </IconButton>
+                </div>
+                <text>{orgItem.title}</text>
+                <LinkButton
+                  href={`/dashboard/organization/${orgItem.slug}/dataset`}
+                >
+                  Manage Datasets
+                </LinkButton>
+                <LinkButton
+                  href={`/dashboard/organization/${orgItem.slug}/consumers`}
+                >
+                  Manage Consumers
+                </LinkButton>
               </div>
-              <text>{orgItem.title}</text>
-              <LinkButton
-                href={`/dashboard/organization/${orgItem.slug}/dataset`}
-              >
-                Manage Datasets
-              </LinkButton>
-              <LinkButton
-                href={`/dashboard/organization/${orgItem.slug}/consumers`}
-              >
-                Manage Consumers
-              </LinkButton>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Page;
