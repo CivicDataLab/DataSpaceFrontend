c1935ceb1ff62359a4182d0fb276897c105b619e
aashimgarg <aashim1garg@gmail.com>
2024-04-18 14:27:26 +0530

    fix header for org dashboard

--- app/[locale]/dashboard/organization/[organizationId]/dataset/page-layout.tsx
+++ app/[locale]/dashboard/organization/[organizationId]/dataset/page-layout.tsx
@@ -1,11 +1,20 @@
 'use client';
 
+import { useParams, useRouter } from 'next/navigation';
 import { graphql } from '@/gql';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useParams, useRouter } from 'next/navigation';
-import { Divider } from 'opub-ui';
+import {
+  Divider,
+  Icon,
+  Tab,
+  TabList,
+  TabPanel,
+  Tabs,
+  Text,
+} from 'opub-ui';
 
 import { GraphQL } from '@/lib/api';
+import { Icons } from '@/components/icons';
 import { ActionBar } from './components/action-bar';
 import { Content } from './components/content';
 
@@ -26,7 +35,6 @@ const createDatasetMutationDoc: any = graphql(`
     }
   }
 `);
-
 
 export const Page = () => {
   const params = useParams();
@@ -52,13 +60,37 @@ export const Page = () => {
     }
   );
 
+  const DATASET_INFO = {
+    title: 'Untitled',
+    timestamp: '20 March 2023 - 10:30AM',
+  };
+
+  const TabsList = [
+    {
+      value: 'Resources',
+      label: 'Resources',
+    },
+    {
+      value: 'Access Models',
+      label: 'Access Models',
+    },
+    {
+      value: 'Dataset Metadata',
+      label: 'Dataset Metadata',
+    },
+    {
+      value: 'Publish',
+      label: 'Publish',
+    },
+  ];
+
   // React.useEffect(() => {
   //   router.prefetch('/dashboard/dataset/new');
   // }, []);
 
   return (
     <>
-      <ActionBar
+      {/* <ActionBar
         title="My Datasets"
         primaryAction={{
           content: 'Add New Dataset',
@@ -67,8 +99,41 @@ export const Page = () => {
           },
         }}
         isLoading={isLoading}
-      />
+      /> */}
+      <div className="flex justify-between py-5">
+        <div className="flex items-center gap-4">
+          <Text variant="headingSm" color="subdued">
+            DATASET NAME : {DATASET_INFO.title} - {DATASET_INFO.timestamp}
+          </Text>
+          <Text variant="headingSm" className="flex items-center gap-1" color="interactive">
+            <Icon
+              source={Icons.datasetSettings}
+              size={16}
+              color="interactive"
+            />
+            edit
+          </Text>
+        </div>
+
+        <Text className="flex gap-1" color="interactive">
+          Go back to Drafts{' '}
+          <Icon source={Icons.cross} size={20} color="interactive" />
+        </Text>
+      </div>
       <Divider />
+
+      <div className="mt-5">
+        <Tabs defaultValue="Resources">
+          <TabList fitted>
+            {TabsList.map((item, index) => (
+              <Tab value={item.value} key={index}>
+                {item.label}
+              </Tab>
+            ))}
+          </TabList>
+        </Tabs>
+      </div>
+
       <Content />
     </>
   );
