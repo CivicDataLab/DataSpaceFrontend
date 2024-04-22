3f42a79b902086dc01d008282dfd1f528a03ff15
aashimgarg <aashim1garg@gmail.com>
2024-04-22 12:45:40 +0530

    fix dataset route

--- app/[locale]/dashboard/organization/[organizationId]/dataset/[id]/page.tsx
+++ app/[locale]/dashboard/organization/[organizationId]/dataset/[id]/page.tsx
@@ -1,12 +1,16 @@
 'use client';
 
 import React from 'react';
-import { notFound } from 'next/navigation';
+import Link from 'next/link';
+import { notFound, useParams } from 'next/navigation';
 import { usePRouter } from '@/hooks/use-prouter';
+import { Divider, Icon, Tab, TabList, TabPanel, Tabs, Text } from 'opub-ui';
 
 import { testDataset } from '@/config/dashboard';
+import { Icons } from '@/components/icons';
 import { InProgress } from '@/components/in-progress';
 import { ActionBar } from '../components/action-bar';
+import { UploadResource } from '../components/upload-resources';
 
 export default function Page({ params }: { params: { id: string } }) {
   const router = usePRouter();
@@ -15,21 +19,94 @@ export default function Page({ params }: { params: { id: string } }) {
   // }, []);
 
   // get demo data
-  const data = testDataset[params.id];
-  if (!data) {
-    notFound();
-  }
+
+  const param = useParams<{ organizationId: string }>();
+
+  const DATASET_INFO = {
+    title: 'Untitled',
+    timestamp: '20 March 2023 - 10:30AM',
+  };
+
+  const TabsList = [
+    {
+      value: 'Resources',
+      label: 'Resources',
+      component: <UploadResource />,
+    },
+    {
+      value: 'Access Models',
+      label: 'Access Models',
+      component: <UploadResource />,
+    },
+    {
+      value: 'Dataset Metadata',
+      label: 'Dataset Metadata',
+      component: <UploadResource />,
+    },
+    {
+      value: 'Publish',
+      label: 'Publish',
+      component: <UploadResource />,
+    },
+  ];
+  // const data = testDataset[params.id];
+  // if (!data) {
+  //   notFound();
+  // }
 
   return (
     <div className="flex h-full flex-col">
-      <ActionBar
+      {/* <ActionBar
         title={data.name}
         primaryAction={{
           content: 'Add New Dataset',
           onAction: () => router.push(`/dashboard/dataset/${params.id}/edit`),
         }}
-      />
-      <InProgress />
+      /> */}
+      <div className="flex justify-between py-5">
+        <div className="flex items-center gap-4">
+          <Text variant="headingSm" color="subdued">
+            DATASET NAME :
+            <b>
+              {DATASET_INFO.title} - {DATASET_INFO.timestamp}
+            </b>
+          </Text>
+          <Text
+            variant="headingSm"
+            className="flex items-center"
+            color="interactive"
+          >
+            <Icon source={Icons.pencil} size={16} color="interactive" />
+            edit
+          </Text>
+        </div>
+        <Link href={`/dashboard/organization/${param.organizationId}/dataset`}>
+          manage datasests
+          <Text className="flex gap-1" color="interactive">
+            Go back to Drafts{' '}
+            <Icon source={Icons.cross} size={20} color="interactive" />
+          </Text>
+        </Link>
+      </div>
+      <Divider />
+
+      <div className="mt-5">
+        <Tabs defaultValue="Resources">
+          <TabList fitted>
+            {TabsList.map((item, index) => (
+              <Tab value={item.value} key={index}>
+                {item.label}
+              </Tab>
+            ))}
+          </TabList>
+          {TabsList.map((item, index) => (
+            <TabPanel value={item.value} key={index}>
+              {item.component}
+            </TabPanel>
+          ))}
+        </Tabs>
+      </div>
+      {/* <InProgress /> */}
     </div>
   );
 }
--- app/[locale]/dashboard/organization/[organizationId]/dataset/page-layout.tsx
+++ app/[locale]/dashboard/organization/[organizationId]/dataset/page-layout.tsx
@@ -3,18 +3,8 @@
 import { useParams, useRouter } from 'next/navigation';
 import { graphql } from '@/gql';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import {
-  Divider,
-  Icon,
-  Tab,
-  TabList,
-  TabPanel,
-  Tabs,
-  Text,
-} from 'opub-ui';
 
 import { GraphQL } from '@/lib/api';
-import { Icons } from '@/components/icons';
 import { ActionBar } from './components/action-bar';
 import { Content } from './components/content';
 
@@ -60,37 +50,13 @@ export const Page = () => {
     }
   );
 
-  const DATASET_INFO = {
-    title: 'Untitled',
-    timestamp: '20 March 2023 - 10:30AM',
-  };
-
-  const TabsList = [
-    {
-      value: 'Resources',
-      label: 'Resources',
-    },
-    {
-      value: 'Access Models',
-      label: 'Access Models',
-    },
-    {
-      value: 'Dataset Metadata',
-      label: 'Dataset Metadata',
-    },
-    {
-      value: 'Publish',
-      label: 'Publish',
-    },
-  ];
-
   // React.useEffect(() => {
   //   router.prefetch('/dashboard/dataset/new');
   // }, []);
 
   return (
     <>
-      {/* <ActionBar
+      <ActionBar
         title="My Datasets"
         primaryAction={{
           content: 'Add New Dataset',
@@ -99,40 +65,7 @@ export const Page = () => {
           },
         }}
         isLoading={isLoading}
-      /> */}
-      <div className="flex justify-between py-5">
-        <div className="flex items-center gap-4">
-          <Text variant="headingSm" color="subdued">
-            DATASET NAME : {DATASET_INFO.title} - {DATASET_INFO.timestamp}
-          </Text>
-          <Text variant="headingSm" className="flex items-center" color="interactive">
-            <Icon
-              source={Icons.pencil}
-              size={16}
-              color="interactive"
-            />
-            edit
-          </Text>
-        </div>
-
-        <Text className="flex gap-1" color="interactive">
-          Go back to Drafts{' '}
-          <Icon source={Icons.cross} size={20} color="interactive" />
-        </Text>
-      </div>
-      <Divider />
-
-      <div className="mt-5">
-        <Tabs defaultValue="Resources">
-          <TabList fitted>
-            {TabsList.map((item, index) => (
-              <Tab value={item.value} key={index}>
-                {item.label}
-              </Tab>
-            ))}
-          </TabList>
-        </Tabs>
-      </div>
+      />
 
       <Content />
     </>
