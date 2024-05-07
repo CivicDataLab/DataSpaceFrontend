302aa1f9e64395d7252e08484b2592ec2ae5381d
sanjaypinna <sanjaypinna265@gmail.com>
2024-05-07 18:34:25 +0530

    add visualizations

--- app/[locale]/(user)/datasets/[datasetIdentifier]/components/Visualizations/index.tsx
+++ app/[locale]/(user)/datasets/[datasetIdentifier]/components/Visualizations/index.tsx
@@ -1,12 +1,80 @@
 import React from 'react';
-import Image from 'next/image';
 import { Button, Text } from 'opub-ui';
+import { BarChart } from 'opub-ui/viz';
 
 interface VisualizationProps {
   data: any;
 }
 
 const Visualization: React.FC<VisualizationProps> = ({ data }) => {
+  const barOptions1 = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: 'bar',
+        name: 'Sales',
+        color: 'rgb(55,162,218)',
+      },
+    ],
+  };
+  const barOptions2 = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: 'bar',
+        name: 'Sales',
+        color: 'rgb(95, 217, 188)',
+      },
+    ],
+  };
+  const barOptions3 = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: 'bar',
+        name: 'Sales',
+        color: 'rgb(208, 138, 208)',
+      },
+    ],
+  };
+  const barOptions4 = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: 'bar',
+        name: 'Sales',
+        color: 'rgb(166, 89, 120)',
+      },
+    ],
+  };
   return (
     <div className="">
       {data.map((item: any, index: any) => (
@@ -19,17 +87,11 @@ const Visualization: React.FC<VisualizationProps> = ({ data }) => {
           <Button className="h-fit w-fit" kind="secondary">
             Download
           </Button>
-          <div className="flex flex-wrap gap-8">
-            {item.images.map((image: string, index: number) => (
-              <Image
-                key={index}
-                src={image}
-                width={280}
-                className="flex-shrink-0"
-                height={280}
-                alt="visualization"
-              />
-            ))}
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+            <BarChart options={barOptions1} height={'280px'} />
+            <BarChart options={barOptions2} height={'280px'} />
+            <BarChart options={barOptions3} height={'280px'} />
+            <BarChart options={barOptions4} height={'280px'} />
           </div>
         </div>
       ))}
