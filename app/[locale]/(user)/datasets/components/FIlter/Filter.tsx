897706e7a051d8b56d531f70c49d22a342f5b87d
sanjaypinna <sanjaypinna265@gmail.com>
2024-05-08 17:50:19 +0530

    add accordion in filter component

--- app/[locale]/(user)/datasets/components/FIlter/Filter.tsx
+++ app/[locale]/(user)/datasets/components/FIlter/Filter.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
-import { Button, CheckboxGroup, Icon, Text } from 'opub-ui';
+import React, { useState } from 'react';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+  Button,
+  CheckboxGroup,
+  Icon,
+  Text,
+} from 'opub-ui';
 
 import { Icons } from '@/components/icons';
 
@@ -30,16 +39,16 @@ const Filter: React.FC<FilterProps> = ({ setOpen }) => {
       title: 'Sector',
       Options: [
         {
-          label: 'Option1',
-          value: 'Option1',
+          label: 'Option21',
+          value: 'Option21',
         },
         {
-          label: 'Option2',
-          value: 'Option2',
+          label: 'Option22',
+          value: 'Option22',
         },
         {
-          label: 'Option3',
-          value: 'Option3',
+          label: 'Option23',
+          value: 'Option23',
         },
       ],
     },
@@ -47,58 +56,69 @@ const Filter: React.FC<FilterProps> = ({ setOpen }) => {
       title: 'Category',
       Options: [
         {
-          label: 'Option1',
-          value: 'Option1',
+          label: 'Option31',
+          value: 'Option31',
         },
         {
-          label: 'Option2',
-          value: 'Option2',
+          label: 'Option32',
+          value: 'Option32',
         },
         {
-          label: 'Option3',
-          value: 'Option3',
+          label: 'Option33',
+          value: 'Option33',
         },
       ],
     },
   ];
-
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
   return (
-    <>
-      {setOpen && (
-        <div className="flex justify-between">
-          <div>
-            <Text variant="headingMd">Filters</Text>
-          </div>
-
+    <div className="rounded-2 border-2   border-solid border-baseGraySlateSolid5 px-4  py-6">
+      <div className="mb-5 flex justify-between  ">
+        <div>
+          <Text variant="headingMd">Filters</Text>
+        </div>
+        {setOpen && (
           <div className="align-center mr-2">
             <Button onClick={(e) => setOpen(false)} kind="tertiary">
               <Icon source={Icons.cross} size={24} color="default" />
             </Button>
           </div>
-        </div>
-      )}
-      <div className="flex flex-col">
+        )}
+      </div>
+      <div className="flex flex-col gap-5  ">
         {filtersData.map((item, index) => (
           <div key={index}>
-            <div className="bg-borderOnBGSubdued px-3 py-1">
-              <Text variant="headingSm" color="onBgDefault">
-                {item.title}
-              </Text>
-            </div>
-            <div className="bg-iconOnBGSubdued p-3">
-              <CheckboxGroup
-                name="checkbox"
-                options={item.Options.map((Option) => ({
-                  label: Option.label,
-                  value: Option.value,
-                }))}
-                title={undefined}
-              />
-            </div>
+            <Accordion type="single" collapsible className="w-full">
+              <AccordionItem value={item.title}>
+                <AccordionTrigger className="flex w-full flex-wrap items-center gap-2 rounded-1 bg-baseIndigoSolid5 py-2 hover:no-underline ">
+                  <Text>{item.title}</Text>
+                </AccordionTrigger>
+                <AccordionContent
+                  className="flex w-full flex-col px-3 pb-0 pt-2 "
+                  style={{
+                    backgroundColor: 'var( --base-pure-white)',
+                    outline: '1px solid var( --base-pure-white)',
+                  }}
+                >
+                  <CheckboxGroup
+                    name="checkbox"
+                    options={item.Options.map((Option) => ({
+                      label: Option.label,
+                      value: Option.value,
+                    }))}
+                    title={undefined}
+                    value={selectedOptions}
+                    onChange={(e) => {
+                      setSelectedOptions(e), console.log(e);
+                    }}
+                  />
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
