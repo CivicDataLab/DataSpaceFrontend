971fe4c124b0ba3d2d5c9d22ce53ea4b10822807
sanjaypinna <sanjaypinna265@gmail.com>
2024-05-08 17:54:53 +0530

    update datasets page as per hifi design

--- app/[locale]/(user)/datasets/page.tsx
+++ app/[locale]/(user)/datasets/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import React, { useState } from 'react';
-import { Button, SearchInput, Select, Text, Tray } from 'opub-ui';
+import { useState } from 'react';
+import { Button, Pill, SearchInput, Select, Text, Tray } from 'opub-ui';
 
 import BreadCrumbs from '@/components/BreadCrumbs';
-import Cards from './components/Cards';
+import Card from './components/Card';
 import Filter from './components/FIlter/Filter';
 import { data } from './data';
 
@@ -19,25 +19,26 @@ const DatasetsListing = () => {
           { href: '#', label: 'Dataset Listing' },
         ]}
       />
-
-      <section className="row flex gap-7 pt-2">
-        <div className="hidden w-60 lg:block">
-          <Filter />
-        </div>
-        <div className="w-full px-2">
-          <div className="flex flex-wrap items-center justify-between gap-2">
-            <div>
-              <Text>Showing {data.length} Datasets</Text>
-            </div>
-            <div className="w-full max-w-[550px] md:block">
-              <SearchInput
-                label={'Search'}
-                name={'Search'}
-                placeholder="Search for data"
-              />
-            </div>
+      <section className="mx-6 md:mx-8 lg:mx-10 ">
+        <div className="my-4 flex flex-wrap items-center justify-between gap-6 rounded-2 bg-baseBlueSolid4  p-2">
+          <div>
+            <Text>
+              Showing {data.length} of {data.length} Datasets
+            </Text>
+          </div>
+          <div className="w-full max-w-[550px] md:block">
+            <SearchInput
+              label={'Search'}
+              name={'Search'}
+              placeholder="Search for data"
+            />
+          </div>
+          <div className="flex items-center gap-2">
+            <Text variant="bodyLg" className=" font-bold text-baseBlueSolid8">
+              Sort by:
+            </Text>
             <Select
-              label="Sort by "
+              label=""
               labelInline
               name="select"
               options={[
@@ -51,27 +52,39 @@ const DatasetsListing = () => {
                 },
               ]}
             />
-
-            <Tray
-              size="narrow"
-              open={open}
-              onOpenChange={setOpen}
-              trigger={
-                <Button
-                  kind="secondary"
-                  className=" lg:hidden"
-                  onClick={(e) => setOpen(true)}
-                >
-                  Filter
-                </Button>
-              }
-            >
-              <Filter setOpen={setOpen} />
-            </Tray>
           </div>
-          {data.map((item, index) => (
-            <Cards key={index} data={item} />
-          ))}
+          <Tray
+            size="narrow"
+            open={open}
+            onOpenChange={setOpen}
+            trigger={
+              <Button
+                kind="secondary"
+                className=" lg:hidden"
+                onClick={(e) => setOpen(true)}
+              >
+                Filter
+              </Button>
+            }
+          >
+            <Filter setOpen={setOpen} />
+          </Tray>
+        </div>
+        <div className="row flex gap-5">
+          <div className=" hidden min-w-64 lg:block">
+            <Filter />
+          </div>
+          <div className="flex w-full flex-col  px-2 ">
+            <div className=" flex gap-2 border-b-2 border-solid border-baseGraySlateSolid4 pb-4">
+              <Pill onRemove={(e) => console.log('test')}>CSV</Pill>
+              <Pill onRemove={(e) => console.log('test')}>PDF</Pill>
+            </div>
+            <div className="mb-16 flex flex-col gap-6">
+              {data.map((item, index) => (
+                <Card key={index} data={item} />
+              ))}
+            </div>
+          </div>
         </div>
       </section>
     </main>
