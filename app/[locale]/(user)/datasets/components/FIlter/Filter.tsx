d4ef50fc8efe635986f3689648b2056d7701741a
sanjaypinna <sanjaypinna265@gmail.com>
2024-05-09 15:37:46 +0530

    modify filter component

--- app/[locale]/(user)/datasets/components/FIlter/Filter.tsx
+++ app/[locale]/(user)/datasets/components/FIlter/Filter.tsx
@@ -14,102 +14,61 @@ import { Icons } from '@/components/icons';
 
 interface FilterProps {
   setOpen?: (isOpen: boolean) => void;
+  options?: Record<string, any>;
+  setSelectedOptions: React.Dispatch<
+    React.SetStateAction<Record<string, string[]>>
+  >;
+  selectedOptions: Record<string, string[]>;
 }
 
-const Filter: React.FC<FilterProps> = ({ setOpen }) => {
-  const filtersData = [
-    {
-      title: 'Organization',
-      Options: [
-        {
-          label: 'Option1',
-          value: 'Option1',
-        },
-        {
-          label: 'Option2',
-          value: 'Option2',
-        },
-        {
-          label: 'Option3',
-          value: 'Option3',
-        },
-      ],
-    },
-    {
-      title: 'Sector',
-      Options: [
-        {
-          label: 'Option21',
-          value: 'Option21',
-        },
-        {
-          label: 'Option22',
-          value: 'Option22',
-        },
-        {
-          label: 'Option23',
-          value: 'Option23',
-        },
-      ],
-    },
-    {
-      title: 'Category',
-      Options: [
-        {
-          label: 'Option31',
-          value: 'Option31',
-        },
-        {
-          label: 'Option32',
-          value: 'Option32',
-        },
-        {
-          label: 'Option33',
-          value: 'Option33',
-        },
-      ],
-    },
-  ];
-  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+const Filter: React.FC<FilterProps> = ({
+  setOpen,
+  options,
+  setSelectedOptions,
+  selectedOptions,
+}) => {
   return (
-    <div className="rounded-2 border-2   border-solid border-baseGraySlateSolid5 px-4  py-6">
-      <div className="mb-5 flex justify-between  ">
+    <div className="rounded-2 border-2 border-solid border-baseGraySlateSolid5 px-4 py-6">
+      <div className="mb-5 flex justify-between">
         <div>
           <Text variant="headingMd">Filters</Text>
         </div>
         {setOpen && (
           <div className="align-center mr-2">
-            <Button onClick={(e) => setOpen(false)} kind="tertiary">
+            <Button onClick={() => setOpen(false)} kind="tertiary">
               <Icon source={Icons.cross} size={24} color="default" />
             </Button>
           </div>
         )}
       </div>
-      <div className="flex flex-col gap-5  ">
-        {filtersData.map((item, index) => (
+      <div className="flex flex-col gap-5">
+        {Object.entries(options || {}).map(([category, data], index) => (
           <div key={index}>
             <Accordion type="single" collapsible className="w-full">
-              <AccordionItem value={item.title}>
-                <AccordionTrigger className="flex w-full flex-wrap items-center gap-2 rounded-1 bg-baseIndigoSolid5 py-2 hover:no-underline ">
-                  <Text>{item.title}</Text>
+              <AccordionItem value={category}>
+                <AccordionTrigger className="flex w-full flex-wrap items-center gap-2 rounded-1 bg-baseIndigoSolid5 py-2 hover:no-underline">
+                  <Text>{category}</Text>
                 </AccordionTrigger>
                 <AccordionContent
-                  className="flex w-full flex-col px-3 pb-0 pt-2 "
+                  className="flex w-full flex-col px-3 pb-0 pt-2"
                   style={{
-                    backgroundColor: 'var( --base-pure-white)',
-                    outline: '1px solid var( --base-pure-white)',
+                    backgroundColor: 'var(--base-pure-white)',
+                    outline: '1px solid var(--base-pure-white)',
                   }}
                 >
                   <CheckboxGroup
-                    name="checkbox"
-                    options={item.Options.map((Option) => ({
-                      label: Option.label,
-                      value: Option.value,
+                    name={category}
+                    options={data?.buckets?.map((bucket: { key: string }) => ({
+                      label: bucket.key,
+                      value: bucket.key,
                     }))}
                     title={undefined}
-                    value={selectedOptions}
+                    value={selectedOptions[category] || []}
                     onChange={(e) => {
-                      setSelectedOptions(e), console.log(e);
+                      setSelectedOptions({
+                        ...selectedOptions,
+                        [category]: e,
+                      });
                     }}
                   />
                 </AccordionContent>
