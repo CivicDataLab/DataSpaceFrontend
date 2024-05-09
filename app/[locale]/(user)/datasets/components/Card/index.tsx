1606159cd8f1e1f7872278d5f848770e080e15b3
sanjaypinna <sanjaypinna265@gmail.com>
2024-05-09 15:38:39 +0530

    modify card component

--- app/[locale]/(user)/datasets/components/Card/index.tsx
+++ app/[locale]/(user)/datasets/components/Card/index.tsx
@@ -5,9 +5,14 @@ import { Tag, Text } from 'opub-ui';
 import CustomTags from '@/components/CustomTags';
 
 interface Dataset {
-  datasetTitle: string;
-  description: string;
-  id: string;
+  _source: {
+    dataset_title: string;
+    dataset_description: string;
+    dataset_access_models: Array<[]>;
+    sector: Array<[]>;
+    format: Array<[]>;
+  };
+  _id: number;
   metadata: Metadata;
 }
 
@@ -21,44 +26,55 @@ interface Metadata {
 }
 
 const Cards = ({ data }: { data: Dataset }) => {
-  const { datasetTitle, description, metadata, id } = data;
+  const {
+    dataset_title,
+    dataset_description,
+    dataset_access_models,
+    sector,
+    format,
+  } = data?._source || {};
+
+  const { _id } = data;
+
   return (
     <>
       <div className="border-b-2 border-solid border-baseGraySlateSolid4 p-6 ">
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
           <div className="">
-            <Link href={`/datasets/${id}`}>
-              <Text variant="headingMd">{datasetTitle}</Text>
+            <Link href={`/datasets/${_id}`}>
+              <Text variant="headingMd">{dataset_title}</Text>
             </Link>
           </div>
-          <div className="">
-            <Text variant="bodySm">{description}</Text>
+          <div className="description-container line-clamp-4">
+            <Text variant="bodySm">{dataset_description}</Text>
           </div>
           <div className="flex flex-wrap items-center gap-2">
             <Text fontWeight="bold">Tags:</Text>
             <div className="flex gap-2">
-              {metadata.tags.map((item, index) => (
+              {/* {tags.map((item, index) => (
                 <Tag key={index}>{item.title}</Tag>
-              ))}
+              ))} */}
+              <Tag>Most Viewed</Tag>
+              <Tag>HVD</Tag>
             </div>
           </div>
           <div className="flex flex-wrap items-center">
             <Text fontWeight="bold">Categories&nbsp;:</Text>
-            <Text>&nbsp;{metadata.category}</Text>
+            <Text>&nbsp;{sector}</Text>
           </div>
           <div className="flex flex-wrap items-center gap-2">
             <Text fontWeight="bold">Formats:</Text>
             <div className="flex gap-2">
-              {metadata.formats.map((item, index) => (
-                <Tag key={index}>{item.type}</Tag>
+              {format.map((item, index) => (
+                <Tag key={index}>{item}</Tag>
               ))}
             </div>
           </div>
           <div className="flex flex-wrap items-center">
             <Text fontWeight="bold">Access Models&nbsp;:</Text>
-            <Text>&nbsp;{metadata.accessModelsCount} in total</Text>&nbsp;
+            <Text>&nbsp;{dataset_access_models.length} in total</Text>&nbsp;
             <div className="flex gap-2">
-              {metadata.accessModels.map((item, index) => (
+              {dataset_access_models.map((item: any, index) => (
                 <CustomTags
                   key={index}
                   type={item.type}
