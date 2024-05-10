af74c317805ecc3fb1e9b5c1965cb19e3f233e84
sanjaypinna <sanjaypinna265@gmail.com>
2024-05-10 15:25:41 +0530

    add resource query

--- app/[locale]/(user)/datasets/[datasetIdentifier]/components/Resources/index.tsx
+++ app/[locale]/(user)/datasets/[datasetIdentifier]/components/Resources/index.tsx
@@ -1,5 +1,8 @@
 import { table } from 'console';
 import React from 'react';
+import { useParams } from 'next/navigation';
+import { graphql } from '@/gql';
+import { useQuery } from '@tanstack/react-query';
 import {
   Accordion,
   AccordionContent,
@@ -7,15 +10,13 @@ import {
   AccordionTrigger,
   Button,
   Dialog,
+  Spinner,
   Table,
   Text,
 } from 'opub-ui';
 
+import { GraphQL } from '@/lib/api';
 import ResourceTable from '../../../components/ResourceTable';
-
-interface ResourceProps {
-  data: any;
-}
 
 const generateColumnData = () => {
   return [
@@ -66,77 +67,106 @@ const generateTableData = (accessModelData: any[]) => {
   }));
 };
 
-const Resources: React.FC<ResourceProps> = ({ data }) => {
+const datasetResourceQuery = graphql(`
+  query datasetResources($datasetId: UUID!) {
+    datasetResources(datasetId: $datasetId) {
+      id
+      created
+      modified
+      type
+      name
+      description
+    }
+  }
+`);
+
+const Resources = () => {
+  const params = useParams();
+
+  const { data, isLoading } = useQuery(
+    [`resources_${params.datasetIdentifier}`],
+    () => GraphQL(datasetResourceQuery, { datasetId: params.datasetIdentifier })
+  );
+
+  console.log(data);
+
   return (
     <>
-      {data?.resources.map((item: any, index: any) => (
-        <div
-          key={index}
-          className="my-4 flex flex-col gap-4 rounded-2 p-6 shadow-basicDeep"
-        >
-          <div className="mb-1 flex flex-wrap justify-between gap-1 lg:gap-0">
-            <div className="p2-4 lg:w-2/5">
-              <Text variant="headingMd">{item.name}</Text>
-            </div>
-            <div className="lg:w-3/5 lg:pl-4">
-              <Text>{item.description}</Text>
-            </div>
-          </div>
-          <div className="align-center flex flex-col justify-between gap-4 sm:flex-row">
-            <Dialog>
-              <Dialog.Trigger>
-                <Button className="h-fit w-fit" kind="secondary">
-                  View Fields
-                </Button>
-              </Dialog.Trigger>
-              <Dialog.Content title={'View Fields'}>
-                <Table
-                  columns={[
-                    {
-                      accessorKey: 'title',
-                      header: 'Title',
-                    },
-                    {
-                      accessorKey: 'description',
-                      header: 'Description',
-                    },
-                  ]}
-                  rows={[
-                    { title: 'Res 1', description: 'Desc 1' },
-                    { title: 'Res 2', description: 'Desc 2' },
-                  ]}
-                  hideFooter={true}
-                />
-              </Dialog.Content>
-            </Dialog>
-          </div>
-          <div className="flex">
-            <Accordion type="single" collapsible className="w-full">
-              <AccordionItem value={`item-${index}`}>
-                <AccordionTrigger className="flex w-full flex-wrap items-center gap-2 ">
-                  <div className=" text-baseBlueSolid8 hover:no-underline ">
-                    See Access Type
-                  </div>
-                </AccordionTrigger>
-                <AccordionContent
-                  className="flex w-full flex-col "
-                  style={{
-                    backgroundColor: 'var( --base-pure-white)',
-                    outline: '1px solid var( --base-pure-white)',
-                  }}
-                >
-                  {item.accessModelData && item.accessModelData.length > 0 && (
-                    <ResourceTable
-                      ColumnsData={generateColumnData()}
-                      RowsData={generateTableData(item.accessModelData)}
-                    />
-                  )}
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
-          </div>
+      {isLoading ? (
+        <div className=" my-7">
+          <Spinner />
         </div>
-      ))}
+      ) : (
+        data?.datasetResources?.map((item: any, index: any) => (
+          <div
+            key={index}
+            className="my-4 flex flex-col gap-4 rounded-2 p-6 shadow-basicDeep"
+          >
+            <div className="mb-1 flex flex-wrap justify-between gap-1 lg:gap-0">
+              <div className="p2-4 lg:w-2/5">
+                <Text variant="headingMd">{item.name}</Text>
+              </div>
+              <div className="lg:w-3/5 lg:pl-4">
+                <Text>{item.description}</Text>
+              </div>
+            </div>
+            <div className="align-center flex flex-col justify-between gap-4 sm:flex-row">
+              <Dialog>
+                <Dialog.Trigger>
+                  <Button className="h-fit w-fit" kind="secondary">
+                    View Fields
+                  </Button>
+                </Dialog.Trigger>
+                <Dialog.Content title={'View Fields'}>
+                  <Table
+                    columns={[
+                      {
+                        accessorKey: 'title',
+                        header: 'Title',
+                      },
+                      {
+                        accessorKey: 'description',
+                        header: 'Description',
+                      },
+                    ]}
+                    rows={[
+                      { title: 'Res 1', description: 'Desc 1' },
+                      { title: 'Res 2', description: 'Desc 2' },
+                    ]}
+                    hideFooter={true}
+                  />
+                </Dialog.Content>
+              </Dialog>
+            </div>
+            <div className="flex">
+              <Accordion type="single" collapsible className="w-full">
+                <AccordionItem value={`item-${index}`}>
+                  <AccordionTrigger className="flex w-full flex-wrap items-center gap-2 ">
+                    <div className=" text-baseBlueSolid8 hover:no-underline ">
+                      See Access Type
+                    </div>
+                  </AccordionTrigger>
+                  <AccordionContent
+                    className="flex w-full flex-col "
+                    style={{
+                      backgroundColor: 'var( --base-pure-white)',
+                      outline: '1px solid var( --base-pure-white)',
+                    }}
+                  >
+                    {item.accessModelData &&
+                      item.accessModelData.length > 0 && (
+                        <ResourceTable
+                          ColumnsData={generateColumnData()}
+                          RowsData={generateTableData(item.accessModelData)}
+                        />
+                      )}
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </div>
+          </div>
+        ))
+      )}
     </>
   );
 };
