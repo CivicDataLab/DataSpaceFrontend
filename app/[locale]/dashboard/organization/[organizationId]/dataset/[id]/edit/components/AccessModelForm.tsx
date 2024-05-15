c9a8b28e30901ddcc997e6392dcbaea4fcfd0fbd
sanjaypinna <sanjaypinna265@gmail.com>
2024-05-15 21:43:37 +0530

    add basic UI

--- app/[locale]/dashboard/organization/[organizationId]/dataset/[id]/edit/components/AccessModelForm.tsx
+++ app/[locale]/dashboard/organization/[organizationId]/dataset/[id]/edit/components/AccessModelForm.tsx
@@ -44,21 +44,60 @@ const AccessModelForm: React.FC<AccessModelProps> = ({ setQueryList }) => {
   const { data, isLoading } = useQuery([`resourcesList_${params.id}`], () =>
     GraphQL(datasetResourcesQuery, { datasetId: params.id })
   );
-  console.log(data);
 
   const [accessModelData, setAccessModelData] = useState({
     name: '',
     description: '',
     permission: '',
   });
+  const [selectedResources, setSelectedResources] = useState<string[]>([]);
+  const [showSelectAll, setShowSelectAll] = useState(false);
+  const [availableResources, setAvailableResources] = useState<any[]>([]);
+
+  useEffect(() => {
+    if (data) {
+      setAvailableResources(
+        data.datasetResources.filter(
+          (resource) => !selectedResources.includes(resource.id)
+        )
+      );
+    }
+  }, [data, selectedResources]);
+
+  const [resId, setResId] = useState('');
+
+  // Inside handleAddResource function
+  const handleAddResource = (resourceId: string) => {
+    if (resourceId !== '') {
+      setSelectedResources((prev) => [...prev, resourceId]);
+      setResId('');
+      setAvailableResources((prev) =>
+        prev.filter((item) => item.id !== resourceId)
+      ); // Filter out the selected resource
+    }
+  };
+
+  const handleRemoveResource = (resourceId: string) => {
+    setSelectedResources((prev) => prev.filter((id) => id !== resourceId));
+    setResId('');
+  };
+
+  const handleSelectAll = () => {
+    const allResourceIds =
+      data?.datasetResources.map((resource) => resource.id) || [];
+    setSelectedResources(allResourceIds);
+    setShowSelectAll(false);
+  };
+
+  console.log(selectedResources, data);
 
   return (
-    <div className=" rounded-2 border-2 border-solid border-baseGraySlateSolid6 px-6 py-8">
-      <div className=" mb-6 flex flex-wrap justify-between gap-6">
-        <div className="flex w-2/3  flex-wrap items-center gap-2">
+    <div className="rounded-2 border-2 border-solid border-baseGraySlateSolid6 px-6 py-8">
+      <div className="mb-6 flex flex-wrap justify-between gap-6">
+        <div className="flex w-2/3 flex-wrap items-center gap-2">
           <Text>Access Type Name:</Text>
           <Select
-            className=" w-2/3"
+            className="w-2/3"
             labelHidden
             name={'Access Type Name'}
             options={[]}
@@ -67,19 +106,24 @@ const AccessModelForm: React.FC<AccessModelProps> = ({ setQueryList }) => {
         </div>
         <Button
           onClick={(e) =>
-            setAccessModelData({
-              name: '',
-              description: '',
-              permission: '',
-            })
+            setAccessModelData({ name: '', description: '', permission: '' })
           }
         >
           Add New Access Type
         </Button>
+        {showSelectAll ? (
+          <Button onClick={handleSelectAll} kind="tertiary">
+            Select All
+          </Button>
+        ) : (
+          <Button onClick={() => setShowSelectAll(true)} kind="tertiary">
+            Show Select All
+          </Button>
+        )}
         <Button
           onClick={(e) => setQueryList(true)}
           kind="tertiary"
-          className=" flex text-end"
+          className="flex text-end"
         >
           <span className="flex items-center gap-2">
             <Text color="interactive">
@@ -92,7 +136,7 @@ const AccessModelForm: React.FC<AccessModelProps> = ({ setQueryList }) => {
       </div>
       <Divider />
       {isLoading ? (
-        <div className=" mt-8 flex justify-center">
+        <div className="mt-8 flex justify-center">
           <Spinner />
         </div>
       ) : (
@@ -100,91 +144,71 @@ const AccessModelForm: React.FC<AccessModelProps> = ({ setQueryList }) => {
           <div className="text-center">
             <Button>Save Access Type</Button>
           </div>
-          <div className="flex flex-col gap-6">
-            <div className="flex  gap-6">
-              <div className=" w-4/5">
-                <TextField
-                  value={accessModelData.name}
-                  onChange={(e) => {
-                    setAccessModelData((prevState) => ({
-                      ...prevState,
-                      name: e,
-                    }));
-                  }}
-                  label="Access Type Name"
-                  name="name"
-                  required
-                  helpText="To know about best practices for naming Resources go to our User Guide"
-                />
-              </div>
-              <Select
-                name={'permissions'}
-                className=" w-1/6"
-                options={[
-                  {
-                    label: 'Public',
-                    value: 'PUBIC',
-                  },
-                  {
-                    label: 'Protected',
-                    value: 'PROTECTED',
-                  },
-                  {
-                    label: 'Private',
-                    value: 'PRIVATE',
-                  },
-                ]}
-                label={'Permissions'}
-                placeholder="Select"
-                onChange={(e) => {
-                  setAccessModelData((prevState) => ({
-                    ...prevState,
-                    permission: e,
-                  }));
-                }}
-              />
-            </div>
-            <TextField
-              value={accessModelData.description}
-              onChange={(e) => {
-                setAccessModelData((prevState) => ({
-                  ...prevState,
-                  description: e,
-                }));
-              }}
-              label="Description"
-              name="description"
-              multiline={4}
-            />
-          </div>
-          <div className=" flex flex-wrap items-center gap-5">
+          <TextField
+            value={accessModelData.name}
+            onChange={(e) =>
+              setAccessModelData({ ...accessModelData, name: e })
+            }
+            label="Access Type Name"
+            name="name"
+            required
+            helpText="To know about best practices for naming Resources go to our User Guide"
+          />
+          <TextField
+            value={accessModelData.description}
+            onChange={(e) =>
+              setAccessModelData({
+                ...accessModelData,
+                description: e,
+              })
+            }
+            label="Description"
+            name="description"
+            multiline={4}
+          />
+          <Select
+            name={'permissions'}
+            options={[
+              { label: 'Public', value: 'PUBLIC' },
+              { label: 'Protected', value: 'PROTECTED' },
+              { label: 'Private', value: 'PRIVATE' },
+            ]}
+            label={'Permissions'}
+            placeholder="Select"
+            onChange={(e) =>
+              setAccessModelData({ ...accessModelData, permission: e })
+            }
+          />
+          <div className="flex flex-wrap items-center gap-5">
             <Select
               name={'permissions'}
-              className=" w-4/5"
+              className="w-4/5"
               options={
-                data && data.datasetResources
-                  ? data.datasetResources.map((item) => ({
-                      label: item.name,
-                      value: item.id,
-                    }))
-                  : []
+                availableResources.length > 0
+                  ? [
+                      { label: 'Select', value: '' },
+                      ...availableResources.map((item) => ({
+                        label: item.name,
+                        value: item.id,
+                      })),
+                    ]
+                  : [{ label: 'Select', value: '' }] // Render only 'Select' when no resources are available
               }
               label={'Select the Resources to be added to the Access Type'}
-              placeholder="Select"
-              onChange={(e) => {
-                console.log(e);
-              }}
+              onChange={(e) => setResId(e)}
               required
+              value={resId}
               helpText="Only Resources added will be part of this Access Type. After adding select the Fields and Rows to be included"
             />
+
             <div className="flex h-fit w-fit items-center gap-5">
-              <Button kind="secondary">
+              <Button onClick={() => handleAddResource(resId)} kind="secondary">
                 <span className="flex items-center gap-1">
                   <Text>Add</Text>
                   <Icon source={Icons.plus} size={24} />
                 </span>
               </Button>
-              <Button kind="secondary">
+              <Button onClick={handleSelectAll} kind="secondary">
                 <span className="flex items-center gap-1">
                   <Text>Select All</Text>
                   <Icon source={Icons.plus} size={24} />
@@ -192,14 +216,19 @@ const AccessModelForm: React.FC<AccessModelProps> = ({ setQueryList }) => {
               </Button>
             </div>
           </div>
-          <div>
-            <Combobox
-              name={''}
-              list={[]}
-              label={'Select Field of the resources'}
-              displaySelected
-            />
-          </div>
+          {selectedResources.map((resourceId, index) => (
+            <div key={index} className="flex flex-wrap items-center gap-5">
+              <Combobox
+                name={''}
+                list={[]}
+                label={`Resource ${index + 1}`}
+                displaySelected
+              />
+              <Button onClick={() => handleRemoveResource(resourceId)}>
+                Remove
+              </Button>
+            </div>
+          ))}
         </div>
       )}
     </div>
