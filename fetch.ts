f02a45927d9a2092cd8a7f6af913b9dd9029a32f
sanjaypinna <sanjaypinna265@gmail.com>
2024-05-17 19:48:27 +0530

    add facets api

--- fetch.ts
+++ fetch.ts
@@ -1,14 +1,6 @@
 export const fetchDatasets = async (variables: any) => {
   const response = await fetch(
-    `https://dev.backend.idp.civicdatalab.in/facets/${variables}`
-  );
-  const data = await response.json();
-  return data;
-};
-
-export const fetchFacets = async () => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/search/dataset/`
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/search/dataset/${variables}`
   );
   const data = await response.json();
   return data;
