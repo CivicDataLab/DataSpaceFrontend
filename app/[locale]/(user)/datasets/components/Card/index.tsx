1063aa0ec1f97ea72820726bb6914d6ffed3fd7e
sanjaypinna <sanjaypinna265@gmail.com>
2024-05-17 19:48:46 +0530

    fix cards as per the data

--- app/[locale]/(user)/datasets/components/Card/index.tsx
+++ app/[locale]/(user)/datasets/components/Card/index.tsx
@@ -5,15 +5,9 @@ import { Tag, Text } from 'opub-ui';
 import CustomTags from '@/components/CustomTags';
 
 interface Dataset {
-  _source: {
-    dataset_title: string;
-    dataset_description: string;
-    dataset_access_models: Array<[]>;
-    sector: Array<[]>;
-    format: Array<[]>;
-  };
-  _id: number;
-  metadata: Metadata;
+  description: string;
+  title: string;
+  id: any;
 }
 
 interface Metadata {
@@ -26,39 +20,28 @@ interface Metadata {
 }
 
 const Cards = ({ data }: { data: Dataset }) => {
-  const {
-    dataset_title,
-    dataset_description,
-    dataset_access_models,
-    sector,
-    format,
-  } = data?._source || {};
-
-  const { _id } = data;
-
   return (
     <>
       <div className="border-b-2 border-solid border-baseGraySlateSolid4 p-6 ">
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
           <div className="">
-            <Link href={`/datasets/${_id}`}>
-              <Text variant="headingMd">{dataset_title}</Text>
+            <Link href={`/datasets/${data.id}`}>
+              <Text variant="headingMd">
+                {data.title} = {data.id}
+              </Text>
             </Link>
           </div>
           <div className="description-container line-clamp-4">
-            <Text variant="bodySm">{dataset_description}</Text>
+            <Text variant="bodySm">{data.description}</Text>
           </div>
-          <div className="flex flex-wrap items-center gap-2">
+          {/* <div className="flex flex-wrap items-center gap-2">
             <Text fontWeight="bold">Tags:</Text>
             <div className="flex gap-2">
-              {/* {tags.map((item, index) => (
-                <Tag key={index}>{item.title}</Tag>
-              ))} */}
               <Tag>Most Viewed</Tag>
               <Tag>HVD</Tag>
             </div>
           </div>
-          <div className="flex flex-wrap items-center">
+           <div className="flex flex-wrap items-center">
             <Text fontWeight="bold">Categories&nbsp;:</Text>
             <Text>&nbsp;{sector}</Text>
           </div>
@@ -84,7 +67,7 @@ const Cards = ({ data }: { data: Dataset }) => {
                 />
               ))}
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </>
