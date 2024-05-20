469240f702f659f96084968b926895a9121bdd20
sanjaypinna <sanjaypinna265@gmail.com>
2024-05-20 15:23:17 +0530

    revamp cards component

--- app/[locale]/(user)/datasets/components/Card/index.tsx
+++ app/[locale]/(user)/datasets/components/Card/index.tsx
@@ -1,47 +1,38 @@
-import React from 'react';
 import Link from 'next/link';
 import { Tag, Text } from 'opub-ui';
 
-import CustomTags from '@/components/CustomTags';
-
 interface Dataset {
+  tags: string[];
   description: string;
   title: string;
-  id: any;
-}
-
-interface Metadata {
-  update: string;
-  category: string;
-  tags: { title: string }[];
-  formats: { type: string }[];
-  accessModels: { type: string }[];
-  accessModelsCount: string;
+  id: string;
 }
 
 const Cards = ({ data }: { data: Dataset }) => {
   return (
-    <>
-      <div className="border-b-2 border-solid border-baseGraySlateSolid4 p-6 ">
-        <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-          <div className="">
-            <Link href={`/datasets/${data.id}`}>
-              <Text variant="headingMd">
-                {data.title} = {data.id}
-              </Text>
-            </Link>
-          </div>
-          <div className="description-container line-clamp-4">
-            <Text variant="bodySm">{data.description}</Text>
-          </div>
-          {/* <div className="flex flex-wrap items-center gap-2">
+    <div className="border-b-2 border-solid border-baseGraySlateSolid4 p-6">
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+        <div>
+          <Link href={`/datasets/${data.id}`}>
+            <Text variant="headingMd">
+              {data.title} - {data.id}
+            </Text>
+          </Link>
+        </div>
+        <div className="description-container line-clamp-4">
+          <Text variant="bodySm">{data.description}</Text>
+        </div>
+        {data.tags && data.tags.length > 0 && (
+          <div className="flex flex-wrap items-center gap-2">
             <Text fontWeight="bold">Tags:</Text>
             <div className="flex gap-2">
-              <Tag>Most Viewed</Tag>
-              <Tag>HVD</Tag>
+              {data.tags.map((item, index) => (
+                <Tag key={index}>{item}</Tag>
+              ))}
             </div>
           </div>
-           <div className="flex flex-wrap items-center">
+        )}
+        {/*   <div className="flex flex-wrap items-center">
             <Text fontWeight="bold">Categories&nbsp;:</Text>
             <Text>&nbsp;{sector}</Text>
           </div>
@@ -68,9 +59,9 @@ const Cards = ({ data }: { data: Dataset }) => {
               ))}
             </div>
           </div> */}
-        </div>
       </div>
-    </>
+    </div>
   );
 };
+
 export default Cards;
