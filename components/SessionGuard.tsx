1b0625ab4314b00cd5bff904dd1353ae1c209d10
bhavabhuthi <bhavabhuthi@swecha.net>
2024-05-20 14:11:35 +0530

    fix: force login for dashboard  pages only

--- components/SessionGuard.tsx
+++ components/SessionGuard.tsx
@@ -1,12 +1,19 @@
 'use client';
 
 import { ReactNode, useEffect } from 'react';
+import { usePathname } from 'next/navigation';
 import { signIn, useSession } from 'next-auth/react';
 
 export default function SessionGuard({ children }: { children: ReactNode }) {
   const { data } = useSession();
+
+  const pathname = usePathname();
+
   useEffect(() => {
-    if (data?.error === 'RefreshAccessTokenError') {
+    if (
+      data?.error === 'RefreshAccessTokenError' &&
+      pathname.includes('dashboard')
+    ) {
       signIn('keycloak');
     }
   }, [data]);
