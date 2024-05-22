f90620fd5af40c8e41936fce0035e5ab80bf519f
sanjaypinna <sanjaypinna265@gmail.com>
2024-05-22 13:46:41 +0530

    add delete and fix refetch issue

--- app/[locale]/dashboard/organization/[organizationId]/dataset/[id]/edit/components/AccessModelList.tsx
+++ app/[locale]/dashboard/organization/[organizationId]/dataset/[id]/edit/components/AccessModelList.tsx
@@ -1,44 +1,18 @@
+import { UUID } from 'crypto';
 import React, { useEffect } from 'react';
 import { useParams } from 'next/navigation';
 import { graphql } from '@/gql';
-import { useQuery } from '@tanstack/react-query';
-import { Button, Spinner, Table, Text } from 'opub-ui';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { Button, DataTable, IconButton, Spinner, Text, toast } from 'opub-ui';
 
 import { GraphQL } from '@/lib/api';
 import { formatDate, toTitleCase } from '@/lib/utils';
+import { Icons } from '@/components/icons';
 
 interface AccessModelListProps {
   setQueryList: any;
   queryList: any;
 }
-
-const generateColumnData = () => {
-  return [
-    {
-      accessorKey: 'name',
-      header: 'Name of Access Type',
-    },
-    {
-      accessorKey: 'date',
-      header: 'Date Added',
-      cell: ({ row }: any) => {
-        return <Text>{formatDate(row.original.date)}</Text>;
-      },
-    },
-    {
-      accessorKey: 'type',
-      header: 'Permissions',
-    },
-  ];
-};
-
-const generateTableData = (accessModel: any[]) => {
-  return accessModel.map((item: any) => ({
-    name: item.name,
-    date: item.created,
-    type: toTitleCase(item.type.split('.').pop().toLowerCase()),
-  }));
-};
 
 const accessModelQuery = graphql(`
   query accessModelResources($datasetId: UUID!) {
@@ -60,13 +34,23 @@ const accessModelQuery = graphql(`
   }
 `);
 
+const deleteAccessModel: any = graphql(`
+  mutation deleteAccessModel($accessModelId: UUID!) {
+    deleteAccessModel(accessModelId: $accessModelId)
+  }
+`);
+
 const AccessModelList: React.FC<AccessModelListProps> = ({
   setQueryList,
   queryList,
 }) => {
   const params = useParams();
 
-  const { data, isLoading, refetch } = useQuery(
+  const {
+    data,
+    isLoading,
+    refetch,
+  }: { data: any; isLoading: boolean; refetch: any } = useQuery(
     [`accessModelList_${params.id}`],
     () =>
       GraphQL(accessModelQuery, {
@@ -78,6 +62,63 @@ const AccessModelList: React.FC<AccessModelListProps> = ({
     refetch();
   }, [queryList]);
 
+  const { mutate } = useMutation(
+    (data: { accessModelId: UUID }) => GraphQL(deleteAccessModel, data),
+    {
+      onSuccess: () => {
+        toast('Access Model Deleted Successfully');
+        refetch();
+      },
+      onError: (err: any) => {
+        toast(`Received ${err} while deleting Access model `);
+      },
+    }
+  );
+
+  const generateColumnData = () => {
+    return [
+      {
+        accessorKey: 'name',
+        header: 'Name of Access Type',
+      },
+      {
+        accessorKey: 'date',
+        header: 'Date Added',
+        cell: ({ row }: any) => {
+          return <Text>{formatDate(row.original.date)}</Text>;
+        },
+      },
+      {
+        accessorKey: 'type',
+        header: 'Permissions',
+      },
+      {
+        header: 'DELETE',
+        cell: ({ row }: any) => (
+          <div className="text-center">
+            <IconButton
+              size="medium"
+              icon={Icons.delete}
+              color="interactive"
+              onClick={() => mutate({ accessModelId: row.original.id })}
+            >
+              Delete
+            </IconButton>
+          </div>
+        ),
+      },
+    ];
+  };
+
+  const generateTableData = (accessModel: any[]) => {
+    return accessModel?.map((item: any) => ({
+      name: item.name,
+      date: item.created,
+      type: toTitleCase(item.type.split('.').pop().toLowerCase()),
+      id: item.id,
+    }));
+  };
+
   return (
     <div>
       {!data || isLoading ? (
@@ -88,19 +129,20 @@ const AccessModelList: React.FC<AccessModelListProps> = ({
         <>
           <div className=" my-6 flex justify-between px-3 py-4">
             <Text>
-              Showing {data?.accessModelResources.length} Access Types
+              Showing {data.accessModelResources?.length} Access Types
             </Text>
             <Button onClick={(e) => setQueryList(false)}>
               Add Access Type
             </Button>
           </div>
 
-          {data.accessModelResources.length > 0 && (
-            <Table
-              columns={generateColumnData()}
-              rows={generateTableData(data?.accessModelResources)}
-            />
-          )}
+          <DataTable
+            columns={generateColumnData()}
+            rows={generateTableData(data.accessModelResources)}
+            hideSelection
+            truncate
+            hideFooter
+          />
         </>
       )}
     </div>
