1ae59ee3be3d657e0848191193d9fd96ac66facd
sanjaypinna <sanjaypinna265@gmail.com>
2024-09-16 18:56:07 +0530

    add handleredirect function

--- lib/utils.ts
+++ lib/utils.ts
@@ -67,3 +67,15 @@ export const range = (len: number) => {
   }
   return arr;
 };
+
+
+
+export function handleRedirect(event: any, link: any) {
+  event.preventDefault();
+  const confirmation = window.confirm(
+    `You are being redirected to "${link}". `
+  );
+  if (confirmation) {
+    window.open(link, '_blank');
+  }
+}
