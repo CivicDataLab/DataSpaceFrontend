f0489f66e8052062bf0e938691f8a79385aac488
sanjaypinna <sanjaypinna265@gmail.com>
2024-09-17 18:51:10 +0530

    add conditions

--- app/[locale]/(user)/datasets/[datasetIdentifier]/components/Metadata/index.tsx
+++ app/[locale]/(user)/datasets/[datasetIdentifier]/components/Metadata/index.tsx
@@ -1,8 +1,8 @@
-import { Button, Icon, Text } from 'opub-ui';
 import React from 'react';
+import { Button, Icon, Text } from 'opub-ui';
 
-import { Icons } from '@/components/icons';
 import { toTitleCase } from '@/lib/utils';
+import { Icons } from '@/components/icons';
 
 interface MetadataProps {
   data: any;
@@ -14,9 +14,10 @@ const MetadataComponent: React.FC<MetadataProps> = ({ data, setOpen }) => {
     (item: any) =>
       item.metadataItem.label !== 'Source Website' &&
       item.metadataItem.label !== 'Github Repo Link' &&
-      item.metadataItem.label !== 'Source'
+      item.metadataItem.label !== 'Source' &&
+      item.value.trim() !== '' // Ensure the value is not empty
   );
-
+  
   return (
     <div className="rounded-md shadow-md flex flex-col gap-6 bg-surfaceDefault px-8 py-6">
       <div className="flex items-center justify-between">
@@ -42,43 +43,47 @@ const MetadataComponent: React.FC<MetadataProps> = ({ data, setOpen }) => {
             <Text className="text-base">{item.value}</Text>
           </div>
         ))}
-        <div className="flex items-baseline gap-2 border-b-2 border-solid border-baseGraySlateSolid6  pb-2">
-          <Text className="text-base min-w-[120px] basis-1/4 font-medium">
-            Formats:
-          </Text>
-          <div className="flex flex-wrap gap-2">
-            {data?.formats.map((item: any, index: any) => (
-              <Text  key={index}>
-                {item}
-              </Text>
-            ))}
+        {data?.formats.length > 0 && (
+          <div className="flex items-baseline gap-2 border-b-2 border-solid border-baseGraySlateSolid6  pb-2">
+            <Text className="text-base min-w-[120px] basis-1/4 font-medium">
+              Formats:
+            </Text>
+            <div className="flex flex-wrap gap-2">
+              {data?.formats.map((item: any, index: any) => (
+                <Text key={index}>{item}</Text>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="flex items-baseline gap-2 border-b-2 border-solid border-baseGraySlateSolid6  pb-2">
-          <Text className="text-base min-w-[120px] basis-1/4 font-medium">
-            Category:
-          </Text>
-          <div className="flex flex-wrap gap-2">
-            {data?.categories.map((item: any, index: any) => (
-              <Text className=" underline" key={index}>
-                {item.name}
-              </Text>
-            ))}
+        )}
+        {data?.categories.length > 0 && (
+          <div className="flex items-baseline gap-2 border-b-2 border-solid border-baseGraySlateSolid6  pb-2">
+            <Text className="text-base min-w-[120px] basis-1/4 font-medium">
+              Category:
+            </Text>
+            <div className="flex flex-wrap gap-2">
+              {data?.categories.map((item: any, index: any) => (
+                <Text className=" underline" key={index}>
+                  {item.name}
+                </Text>
+              ))}
+            </div>
           </div>
-        </div>
+        )}
 
-        <div className="flex items-baseline gap-2  pb-2">
-          <Text className="text-base min-w-[120px] basis-1/4 font-medium">
-            Tags:
-          </Text>
-          <div className="flex flex-wrap gap-2">
-            {data?.tags.map((item: any, index: any) => (
-              <Text className=" underline" key={index}>
-                {item.value}
-              </Text>
-            ))}
+        {data?.tags.length > 0 && (
+          <div className="flex items-baseline gap-2  pb-2">
+            <Text className="text-base min-w-[120px] basis-1/4 font-medium">
+              Tags:
+            </Text>
+            <div className="flex flex-wrap gap-2">
+              {data?.tags.map((item: any, index: any) => (
+                <Text className=" underline" key={index}>
+                  {item.value}
+                </Text>
+              ))}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
