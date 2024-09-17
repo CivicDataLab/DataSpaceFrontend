c25f7bf211ecac7e9bdbf9c0f19a471265aea033
sanjaypinna <sanjaypinna265@gmail.com>
2024-09-17 17:36:08 +0530

    fix layout

--- app/[locale]/(user)/datasets/components/Card/index.tsx
+++ app/[locale]/(user)/datasets/components/Card/index.tsx
@@ -58,53 +58,108 @@ const Cards = ({ data }: { data: Dataset }) => {
     <div className="mb-6 border-b-2 border-solid border-baseGraySlateSolid4">
       <Link href={`/datasets/${data.id}`} passHref>
         <div className="w-full cursor-pointer rounded-1 bg-surfaceDefault p-6 shadow-elementCard">
-          <div className="flex flex-col flex-wrap items-start gap-6 lg:flex-row">
-            <div className="flex flex-1 flex-col flex-wrap items-start gap-3 p-0">
-              <Text
-                className="text-textSubdued"
-                variant="headingLg"
-                as="p"
-                breakWord
-                fontWeight="semibold"
-              >
-                {data.title}
-              </Text>
-              <Text
-                color="default"
-                className="text-textDefault"
-                variant="headingSm"
-                fontWeight="medium"
-              >
-                Source: {getMetadataValue(data, 'Source') || 'NA'}
-              </Text>
-              <span className="flex flex-col items-start gap-1">
+          <div>
+            <div className="flex flex-col flex-wrap items-start gap-3 lg:flex-row lg:gap-6">
+              <div className="flex  flex-col flex-wrap items-start gap-3 p-0 lg:w-2/5">
                 <Text
-                  color="default"
                   className="text-textSubdued"
-                  variant="bodySm"
-                  fontWeight="regular"
+                  variant="headingLg"
+                  as="p"
+                  breakWord
+                  fontWeight="semibold"
                 >
-                  Last Updated: {getMetadataValue(data, 'Last Updated') || 'NA'}{' '}
-                  | Update Frequency:{' '}
-                  {getMetadataValue(data, 'Update Frequency') || 'NA'}
+                  {data.title}
                 </Text>
                 <Text
                   color="default"
-                  className="text-textSubdued"
-                  variant="bodySm"
-                  fontWeight="regular"
+                  className="text-textDefault"
+                  variant="headingSm"
+                  fontWeight="medium"
                 >
-                  Reference Period:{' '}
-                  {formatDateString(getMetadataValue(data, 'Period From')) ||
-                    'NA'}{' '}
-                  to{' '}
-                  {formatDateString(getMetadataValue(data, 'Period To')) ||
-                    'NA'}
+                  Source: {getMetadataValue(data, 'Source') || 'NA'}
                 </Text>
-              </span>
+                <span className="flex flex-col items-start gap-1">
+                  <div className=" flex flex-col gap-2  lg:flex-row">
+                    <Text
+                      color="default"
+                      className="text-textSubdued"
+                      variant="bodySm"
+                      fontWeight="regular"
+                    >
+                      Last Updated:{' '}
+                      {getMetadataValue(data, 'Last Updated') || 'NA'}
+                    </Text>
+                    <Text
+                      color="default"
+                      className="hidden text-textSubdued  lg:block"
+                      variant="bodySm"
+                      fontWeight="regular"
+                    >
+                      |
+                    </Text>
+                    <Text
+                      color="default"
+                      className="text-textSubdued"
+                      variant="bodySm"
+                      fontWeight="regular"
+                    >
+                      Update Frequency:{' '}
+                      {getMetadataValue(data, 'Update Frequency') || 'NA'}
+                    </Text>
+                  </div>
+                  <Text
+                    color="default"
+                    className="text-textSubdued"
+                    variant="bodySm"
+                    fontWeight="regular"
+                  >
+                    Reference Period:{' '}
+                    {formatDateString(getMetadataValue(data, 'Period From')) ||
+                      'NA'}{' '}
+                    to{' '}
+                    {formatDateString(getMetadataValue(data, 'Period To')) ||
+                      'NA'}
+                  </Text>
+                </span>
+              </div>
 
+              <div className="flex flex-1 flex-col-reverse  gap-4 overflow-hidden lg:max-h-[150px] lg:flex-col">
+                <div>
+                  <div
+                    ref={descriptionRef}
+                    className={!showMore ? ' line-clamp-2 lg:line-clamp-5' : ''}
+                  >
+                    <Tooltip
+                      content={data.description}
+                      align="end"
+                      width="wide"
+                    >
+                      <Text variant="bodyMd" as="p" color="default">
+                        {data.description}
+                      </Text>
+                    </Tooltip>
+                  </div>
+
+                  {/* Only show the "Show more" button on medium and small screens */}
+                  {isDescriptionLong && (
+                    <div className="block md:hidden">
+                      <Button
+                        className="self-start p-2"
+                        onClick={toggleShowMore}
+                        variant="interactive"
+                        size="slim"
+                        kind="tertiary"
+                      >
+                        {showMore ? 'Show less' : 'Show more'}
+                      </Button>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="mt-2 flex flex-wrap gap-3 lg:gap-6">
               {data.formats?.length > 0 && (
-                <span className="flex items-center gap-4 py-1 pr-2">
+                <span className="flex items-center gap-4 py-1 pr-2 lg:w-2/5">
                   {data.formats.map((fileType, index) => (
                     <Tag key={index} background-color="#E1F0FF">
                       {fileType}
@@ -112,45 +167,16 @@ const Cards = ({ data }: { data: Dataset }) => {
                   ))}
                 </span>
               )}
-            </div>
 
-            <div className="flex flex-1 flex-col-reverse  gap-4 overflow-hidden lg:max-h-[150px] lg:flex-col">
-              <div>
-                <div
-                  ref={descriptionRef}
-                  className={!showMore ? ' line-clamp-2 lg:line-clamp-3' : ''}
-                >
-                  <Tooltip content={data.description} align='end' width='wide' >
-                    <Text variant="bodyMd" as="p" color="default">
-                      {data.description}
-                    </Text>
-                  </Tooltip>
-                </div>
-
-                {/* Only show the "Show more" button on medium and small screens */}
-                {isDescriptionLong && (
-                  <div className="block md:hidden">
-                    <Button
-                      className="self-start p-2"
-                      onClick={toggleShowMore}
-                      variant="interactive"
-                      size="slim"
-                      kind="tertiary"
-                    >
-                      {showMore ? 'Show less' : 'Show more'}
-                    </Button>
-                  </div>
-                )}
-              </div>
-
-              <span className="flex gap-2 py-1 pr-2">
-                {data?.categories.length > 0 &&
-                  data?.categories.map((category, index) => (
+              {data?.categories.length > 0 && (
+                <span className="flex gap-2 py-1 pr-2">
+                  {data?.categories.map((category, index) => (
                     <Tag key={index} background-color="#E1F0FF">
                       {category}
                     </Tag>
                   ))}
-              </span>
+                </span>
+              )}
             </div>
           </div>
         </div>
