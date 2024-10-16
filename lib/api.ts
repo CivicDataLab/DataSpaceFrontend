8527bc5597950f2933c0d0197e06bde04f95834f
bhavabhuthi <bhavabhuthi@riseup.net>
2024-10-16 19:01:30 +0530

    add next auth session access token to graphql function

--- lib/api.ts
+++ lib/api.ts
@@ -2,22 +2,31 @@ import React from 'react';
 import { type TypedDocumentNode } from '@graphql-typed-document-node/core';
 import { QueryClient } from '@tanstack/react-query';
 import { request } from 'graphql-request';
+import { getSession } from 'next-auth/react';
 
 import { gqlConfig } from '@/config/site';
 
 // create a wrapper function for graphql-request
 // that will be used by react-query
+
 export async function GraphQL<TResult, TVariables>(
   document: TypedDocumentNode<TResult, TVariables>,
   ...[variables]: TVariables extends Record<string, never> ? [] : [TVariables]
 ) {
+  const session = await getSession();
+
+  const headers = {
+    ...gqlConfig.headers,
+    ...(session ? { Authorization: session?.access_token } : {}),
+  };
+
   const data = await request(
     `${process.env.NEXT_PUBLIC_BACKEND_GRAPHQL_URL}`,
     document,
     {
       ...variables,
     },
-    { ...gqlConfig.headers }
+    headers
   );
   return data;
 }
