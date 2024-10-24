828bc63e47180b346427a384a036245e16970bb4
sanjaypinna <sanjaypinna265@gmail.com>
2024-10-24 13:53:41 +0530

    hide redirection

--- app/[locale]/manage/page.tsx
+++ app/[locale]/manage/page.tsx
@@ -6,17 +6,17 @@ import { useParams, useRouter } from 'next/navigation';
 import { Loading } from '@/components/loading';
 
 const Manage = () => {
-  const params = useParams();
-  const router = useRouter();
+  // const params = useParams();
+  // const router = useRouter();
 
-  useEffect(() => {
-    // Redirect to the Use Cases page
-    router.push(`/${params.locale}/manage/usecases`);
-  }, [router]);
+  // useEffect(() => {
+  //   // Redirect to the Use Cases page
+  //   router.push(`/${params.locale}/manage/usecases`);
+  // }, [router]);
 
   return (
     <div>
-      <Loading />
+      <p>Welcome to Manage</p>
     </div> // Optional: You can show a loading message or spinner here
   );
 };
