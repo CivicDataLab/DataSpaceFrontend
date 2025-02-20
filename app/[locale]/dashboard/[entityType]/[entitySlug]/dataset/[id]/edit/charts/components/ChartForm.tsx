fd1ca9a830678189f6d1542b4370b70b82dbcd0f
sanjaypinna <sanjaypinna265@gmail.com>
2025-02-20 16:35:29 +0530

    add logic to show options based on chart type

--- app/[locale]/dashboard/[entityType]/[entitySlug]/dataset/[id]/edit/charts/components/ChartForm.tsx
+++ app/[locale]/dashboard/[entityType]/[entitySlug]/dataset/[id]/edit/charts/components/ChartForm.tsx
@@ -1,42 +1,8 @@
 import React from 'react';
-import { Checkbox, Select, TextField } from 'opub-ui';
+import { ChartTypes } from '@/gql/generated/graphql';
+import { Button, Checkbox, Select, TextField } from 'opub-ui';
 
-interface ChartOptions {
-  aggregateType: string;
-  regionColumn?: string;
-  showLegend: boolean;
-  timeColumn?: string;
-  valueColumn?: string;
-  xAxisColumn: string;
-  xAxisLabel: string;
-  yAxisColumn: any[];
-  yAxisLabel: string;
-}
-
-interface ChartData {
-  chartId: string;
-  description: string;
-  filters: any[];
-  name: string;
-  options: ChartOptions;
-  resource: string;
-  type: string;
-  chart: any;
-}
-
-interface ResourceSchema {
-  fieldName: string;
-  id: string;
-}
-
-interface Resource {
-  id: string;
-  name: string;
-}
-
-interface ResourceData {
-  datasetResources: Resource[];
-}
+import { ChartData, ResourceData, ResourceSchema } from '../types';
 
 interface ChartFormProps {
   chartData: ChartData;
@@ -55,160 +21,247 @@ const ChartForm: React.FC<ChartFormProps> = ({
   handleResourceChange,
   handleSave,
 }) => {
+  const isAssamChart =
+    chartData.type === ChartTypes.AssamDistrict ||
+    chartData.type === ChartTypes.AssamRc;
+  const isGroupedChart =
+    chartData.type === ChartTypes.GroupedBarVertical ||
+    chartData.type === ChartTypes.GroupedBarHorizontal;
+  const isBarOrLineChart =
+    chartData.type === ChartTypes.BarVertical ||
+    chartData.type === ChartTypes.BarHorizontal ||
+    chartData.type === ChartTypes.Line ||
+    chartData.type === ChartTypes.Multiline;
+
+  const handleYAxisColumnChange = (
+    index: number,
+    field: string,
+    value: any
+  ) => {
+    const newYAxisColumns = [...chartData.options.yAxisColumn];
+    newYAxisColumns[index] = {
+      ...newYAxisColumns[index],
+      [field]: value,
+    };
+    handleChange('options', {
+      ...chartData.options,
+      yAxisColumn: newYAxisColumns,
+    });
+  };
+
+  const addYAxisColumn = () => {
+    const newYAxisColumns = [
+      ...(chartData.options.yAxisColumn ?? []),
+      { fieldName: '', label: '', color: '' },
+    ];
+    handleChange('options', {
+      ...chartData.options,
+      yAxisColumn: newYAxisColumns,
+    });
+  };
+
+  const removeYAxisColumn = (index: number) => {
+    const newYAxisColumns = chartData.options.yAxisColumn.filter(
+      (_, i) => i !== index
+    );
+    handleChange('options', {
+      ...chartData.options,
+      yAxisColumn: newYAxisColumns,
+    });
+  };
+
+console.log(chartData, "data");
+
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <TextField
         onChange={(e) => handleChange('name', e)}
+        label="Name"
         value={chartData.name}
-        label="Chart Name"
         name="name"
-        required
-        helpText="To know about best practices for naming Visualizations go to our User Guide"
         onBlur={() => handleSave(chartData)}
+        required
       />
       <TextField
         onChange={(e) => handleChange('description', e)}
         label="Description"
         value={chartData.description}
         name="description"
-        multiline={4}
         onBlur={() => handleSave(chartData)}
+        required
       />
-      <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
-        <Select
-          name="chartType"
-          value={chartData.type}
-          options={[
-            { label: 'Bar Vertical', value: 'BAR_VERTICAL' },
-            { label: 'Bar Horizontal', value: 'BAR_HORIZONTAL' },
-            {
-              label: 'GROUPED_BAR_VERTICAL',
-              value: 'GROUPED_BAR_VERTICAL',
-            },
-            {
-              label: ' GROUPED_BAR_HORIZONTAL',
-              value: 'GROUPED_BAR_HORIZONTAL',
-            },
-            { label: 'Line', value: 'LINE' },
-            { label: 'Assam District', value: 'ASSAM_DISTRICT' },
-            { label: 'ASSAM RC', value: 'ASSAM_RC' },
-          ]}
-          label="Select Chart Type"
-          defaultValue={'BAR_VERTICAL'}
-          // placeholder="Select"
-          onBlur={() => handleSave(chartData)}
-          onChange={(e) => handleChange('chartType', e)}
-        />
-        <Select
-          name="resource"
-          options={resourceData?.datasetResources.map((resource: any) => ({
-            label: resource.name,
-            value: resource.id,
-          }))}
-          value={chartData?.resource}
-          defaultValue={resourceData?.datasetResources[0]?.id}
-          label="Select Resources"
-          onBlur={() => handleSave(chartData)}
-          onChange={(e) => handleResourceChange(e)}
-        />
-        {chartData.type !== 'ASSAM_DISTRICT' &&
-        chartData.type !== 'ASSAM_RC' ? (
-          <>
-            <Select
-              name="xAxisColumn"
-              options={resourceSchema?.map((field: any) => ({
-                label: field.fieldName,
-                value: field.id,
-              }))}
-              label="X-axis Column"
-              value={chartData.options.xAxisColumn}
-              placeholder="Select"
-              onBlur={() => handleSave(chartData)}
-              onChange={(e) => handleChange('xAxisColumn', e)}
-            />
-            <TextField
-              onChange={(e) => handleChange('xAxisLabel', e)}
-              label="X-axis Label"
-              value={chartData.options.xAxisLabel}
-              name="xAxisLabel"
-              onBlur={() => handleSave(chartData)}
-              required
-            />
-            <Select
-              name="yAxisColumn"
-              options={resourceSchema?.map((field: any) => ({
-                label: field.fieldName,
-                value: field.id,
-              }))}
-              // value={chartData?.options?.yAxisColumn[0]}
-              label="Y-axis Column"
-              onBlur={() => handleSave(chartData)}
-              placeholder="Select"
-              onChange={(e) => handleChange('yAxisColumn', e)}
-            />
-            <TextField
-              onChange={(e) => handleChange('yAxisLabel', e)}
-              label="Y-axis Label"
-              name="yAxisLabel"
-              value={chartData.options.yAxisLabel}
-              onBlur={() => handleSave(chartData)}
-              required
-            />
-          </>
-        ) : (
-          <>
-            <Select
-              name="regionColumn"
-              options={resourceSchema?.map((field: any) => ({
-                label: field.fieldName,
-                value: field.id,
-              }))}
-              label="Region Column"
-              value={chartData.options.regionColumn}
-              placeholder="Select"
-              onBlur={() => handleSave(chartData)}
-              onChange={(e) => handleChange('regionColumn', e)}
-            />
-            <Select
-              name="valueColumn"
-              options={resourceSchema?.map((field: any) => ({
-                label: field.fieldName,
-                value: field.id,
-              }))}
-              value={chartData.options.valueColumn}
-              label="Value Column"
-              onBlur={() => handleSave(chartData)}
-              placeholder="Select"
-              onChange={(e) => handleChange('valueColumn', e)}
-            />
-          </>
-        )}
-        <Select
-          name="aggregateType"
-          options={[
-            { label: 'None', value: 'NONE' },
-            { label: 'Sum', value: 'SUM' },
-            { label: 'Average', value: 'AVERAGE' },
-            { label: 'Count', value: 'COUNT' },
-          ]}
-          label="Aggregate"
-          value={chartData?.options?.aggregateType}
-          defaultValue="SUM"
-          onBlur={() => handleSave(chartData)}
-          onChange={(e) => handleChange('aggregateType', e)}
-        />
-        <div className=" pt-7">
-          <Checkbox
-            name="legend"
-            value={chartData?.options?.showLegend?.toString()}
-            // checked={chartData?.options?.showLegend}
+      <Select
+        name="type"
+        options={Object.values(ChartTypes).map((type) => ({
+          label: type,
+          value: type,
+        }))}
+        label="Chart Type"
+        value={chartData.type}
+        onBlur={() => handleSave(chartData)}
+        onChange={(e) => handleChange('type', e)}
+        placeholder="Select"
+      />
+      <Select
+        name="resource"
+        options={resourceData?.datasetResources?.map((resource) => ({
+          label: resource.name,
+          value: resource.id,
+        }))}
+        label="Resource"
+        value={chartData.resource}
+        onBlur={() => handleSave(chartData)}
+        onChange={handleResourceChange}
+        placeholder="Select"
+      />
+      <Checkbox
+        name="legend"
+        value={chartData.options.showLegend?.toString()}
+        checked={chartData.options.showLegend}
+        onBlur={() => handleSave(chartData)}
+        onChange={(e) =>
+          handleChange('options', {
+            ...chartData.options,
+            showLegend: e,
+          })
+        }
+      >
+        Show Legend
+      </Checkbox>
+
+      {!isAssamChart && (
+        <>
+          <Select
+            name="xAxisColumn"
+            options={resourceSchema?.map((field) => ({
+              label: field.fieldName,
+              value: field.id,
+            }))}
+            label="X-axis Column"
+            value={chartData.options.xAxisColumn}
             onBlur={() => handleSave(chartData)}
-            onChange={(e) => handleChange('showLegend', e)}
-          >
-            Show Legend (Legend values will be taken from resource)
-          </Checkbox>
-        </div>
-      </div>
+            onChange={(e) =>
+              handleChange('options', {
+                ...chartData.options,
+                xAxisColumn: e,
+              })
+            }
+            placeholder="Select"
+          />
+          <TextField
+            onChange={(e) =>
+              handleChange('options', {
+                ...chartData.options,
+                xAxisLabel: e,
+              })
+            }
+            label="X-axis Label"
+            value={chartData.options.xAxisLabel}
+            name="xAxisLabel"
+            onBlur={() => handleSave(chartData)}
+            required
+          />
+          <TextField
+            onChange={(e) =>
+              handleChange('options', {
+                ...chartData.options,
+                yAxisLabel: e,
+              })
+            }
+            label="Y-axis Label"
+            value={chartData.options.yAxisLabel}
+            name="yAxisLabel"
+            onBlur={() => handleSave(chartData)}
+            required
+          />
+
+          {(isBarOrLineChart || isGroupedChart) && (
+            <div className="flex flex-col gap-4">
+              {chartData?.options?.yAxisColumn?.map((column, index) => (
+                <div key={index} className="flex items-end gap-4">
+                  <Select
+                    name={`yAxisColumn-${index}`}
+                    options={resourceSchema?.map((field) => ({
+                      label: field.fieldName,
+                      value: field.id,
+                    }))}
+                    label="Y-axis Column"
+                    value={column.fieldName}
+                    onChange={(e) =>
+                      handleYAxisColumnChange(index, 'fieldName', e)
+                    }
+                    onBlur={() => handleSave(chartData)}
+                    placeholder="Select"
+                  />
+                  <TextField
+                    name={`yAxisLabel-${index}`}
+                    label="Label"
+                    value={column.label}
+                    onChange={(e) => handleYAxisColumnChange(index, 'label', e)}
+                    onBlur={() => handleSave(chartData)}
+                  />
+                  <input
+                    name={`yAxisColor-${index}`}
+                    type="Color"
+                    value={column.color}
+                    onChange={(e: any) => {
+                      handleYAxisColumnChange(index, 'color', e.target.value);
+                    }}
+                    onBlur={() => handleSave(chartData)}
+                  />
+                  {isGroupedChart && index > 0 && (
+                    <Button onClick={() => removeYAxisColumn(index)}>
+                      Remove
+                    </Button>
+                  )}
+                </div>
+              ))}
+              {isGroupedChart && (
+                <Button onClick={addYAxisColumn}>Add Y-axis Column</Button>
+              )}
+            </div>
+          )}
+        </>
+      )}
+
+      {isAssamChart && (
+        <>
+          <Select
+            name="regionColumn"
+            options={resourceSchema?.map((field) => ({
+              label: field.fieldName,
+              value: field.id,
+            }))}
+            label="Region Column"
+            value={chartData.options.regionColumn}
+            onBlur={() => handleSave(chartData)}
+            onChange={(e) =>
+              handleChange('options', {
+                ...chartData.options,
+                regionColumn: e,
+              })
+            }
+            placeholder="Select"
+          />
+          <Select
+            name="valueColumn"
+            options={resourceSchema?.map((field) => ({
+              label: field.fieldName,
+              value: field.id,
+            }))}
+            label="Value Column"
+            value={chartData.options.valueColumn}
+            onBlur={() => handleSave(chartData)}
+            onChange={(e) =>
+              handleChange('options', {
+                ...chartData.options,
+                valueColumn: e,
+              })
+            }
+            placeholder="Select"
+          />
+        </>
+      )}
     </div>
   );
 };
