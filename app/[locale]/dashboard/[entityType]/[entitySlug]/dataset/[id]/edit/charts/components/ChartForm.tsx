d2305efae59c64d22ff09ac90f3ae262083eec6c
sanjaypinna <sanjaypinna265@gmail.com>
2025-02-21 10:52:34 +0530

    fix css

--- app/[locale]/dashboard/[entityType]/[entitySlug]/dataset/[id]/edit/charts/components/ChartForm.tsx
+++ app/[locale]/dashboard/[entityType]/[entitySlug]/dataset/[id]/edit/charts/components/ChartForm.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { ChartTypes } from '@/gql/generated/graphql';
-import { Button, Checkbox, Select, TextField } from 'opub-ui';
+import { Button, Checkbox, Select, Text, TextField } from 'opub-ui';
 
 import { ChartData, ResourceData, ResourceSchema } from '../types';
 
@@ -33,9 +33,11 @@ const ChartForm: React.FC<ChartFormProps> = ({
     chartData.type === ChartTypes.Line ||
     chartData.type === ChartTypes.Multiline;
 
-
   useEffect(() => {
-    if (!chartData.options.yAxisColumn || chartData.options.yAxisColumn.length === 0) {
+    if (
+      !chartData.options.yAxisColumn ||
+      chartData.options.yAxisColumn.length === 0
+    ) {
       handleChange('options', {
         ...chartData.options,
         yAxisColumn: [{ fieldName: '', label: '', color: '' }],
@@ -81,16 +83,16 @@ const ChartForm: React.FC<ChartFormProps> = ({
   };
 
   const updateChartData = (field: string, value: any) => {
-    
     if (field === 'type') {
       const newData = {
         ...chartData,
         type: value,
         options: {
           ...chartData.options,
-          yAxisColumn: chartData.options.yAxisColumn.length > 0 
-            ? chartData.options.yAxisColumn 
-            : [{ fieldName: '', label: '', color: '' }],
+          yAxisColumn:
+            chartData.options.yAxisColumn.length > 0
+              ? chartData.options.yAxisColumn
+              : [{ fieldName: '', label: '', color: '' }],
         },
       };
       handleChange(field, value);
@@ -104,7 +106,6 @@ const ChartForm: React.FC<ChartFormProps> = ({
       handleSave(newData);
     }
   };
-
 
   return (
     <div className="flex flex-col gap-4">
@@ -148,34 +149,40 @@ const ChartForm: React.FC<ChartFormProps> = ({
         onChange={handleResourceChange}
         placeholder="Select"
       />
-      <Checkbox
-        name="legend"
-        value={chartData.options.showLegend?.toString()}
-        checked={chartData.options.showLegend ?? true}
-        onBlur={() => handleSave(chartData)}
-        onChange={(e) =>
-          handleChange('options', {
-            ...chartData.options,
-            showLegend: e,
-          })
-        }
-      >
-        Show Legend
-      </Checkbox>
-      <Select
-        name="aggregateType"
-        options={[
-          { label: 'None', value: 'NONE' },
-          { label: 'Sum', value: 'SUM' },
-          { label: 'Average', value: 'AVERAGE' },
-          { label: 'Count', value: 'COUNT' },
-        ]}
-        label="Aggregate"
-        value={chartData.options.aggregateType}
-        defaultValue="SUM"
-        onBlur={() => handleSave(chartData)}
-        onChange={(e) => handleChange('options', { ...chartData.options, aggregateType: e })}
-      />
+      <div className="grid grid-cols-1 items-center gap-2 lg:grid-cols-2">
+        <Select
+          name="aggregateType"
+          options={[
+            { label: 'None', value: 'NONE' },
+            { label: 'Sum', value: 'SUM' },
+            { label: 'Average', value: 'AVERAGE' },
+            { label: 'Count', value: 'COUNT' },
+          ]}
+          label="Aggregate"
+          value={chartData.options.aggregateType}
+          defaultValue="SUM"
+          onBlur={() => handleSave(chartData)}
+          onChange={(e) =>
+            handleChange('options', { ...chartData.options, aggregateType: e })
+          }
+        />
+        <div className=" m-0 lg:ml-6 lg:mt-6">
+          <Checkbox
+            name="legend"
+            value={chartData.options.showLegend?.toString()}
+            checked={chartData.options.showLegend ?? true}
+            onBlur={() => handleSave(chartData)}
+            onChange={(e) =>
+              handleChange('options', {
+                ...chartData.options,
+                showLegend: e,
+              })
+            }
+          >
+            Show Legend
+          </Checkbox>
+        </div>
+      </div>
       {!isAssamChart && (
         <>
           <Select
@@ -195,78 +202,100 @@ const ChartForm: React.FC<ChartFormProps> = ({
             }
             placeholder="Select"
           />
-          <TextField
-            onChange={(e) =>
-              handleChange('options', {
-                ...chartData.options,
-                xAxisLabel: e,
-              })
-            }
-            label="X-axis Label"
-            value={chartData.options.xAxisLabel}
-            name="xAxisLabel"
-            onBlur={() => handleSave(chartData)}
-            required
-          />
-          <TextField
-            onChange={(e) =>
-              handleChange('options', {
-                ...chartData.options,
-                yAxisLabel: e,
-              })
-            }
-            label="Y-axis Label"
-            value={chartData.options.yAxisLabel}
-            name="yAxisLabel"
-            onBlur={() => handleSave(chartData)}
-            required
-          />
+          <div className="grid grid-cols-1 gap-2 lg:grid-cols-2">
+            <TextField
+              onChange={(e) =>
+                handleChange('options', {
+                  ...chartData.options,
+                  xAxisLabel: e,
+                })
+              }
+              label="X-axis Label"
+              value={chartData.options.xAxisLabel}
+              name="xAxisLabel"
+              onBlur={() => handleSave(chartData)}
+              required
+            />
+            <TextField
+              onChange={(e) =>
+                handleChange('options', {
+                  ...chartData.options,
+                  yAxisLabel: e,
+                })
+              }
+              label="Y-axis Label"
+              value={chartData.options.yAxisLabel}
+              name="yAxisLabel"
+              onBlur={() => handleSave(chartData)}
+              required
+            />
+          </div>
 
           {(isBarOrLineChart || isGroupedChart) && (
-            <div className="flex flex-col gap-4">
-              {chartData?.options?.yAxisColumn?.map((column, index) => (
-                <div key={index} className="flex items-end gap-4">
-                  <Select
-                    name={`yAxisColumn-${index}`}
-                    options={resourceSchema
-                      ?.filter(field => field.format.toUpperCase() === 'INTEGER' )
-                      .map((field) => ({
-                        label: field.fieldName,
-                        value: field.id,
-                      }))}
-                    label="Y-axis Column"
-                    value={column.fieldName}
-                    onChange={(e) =>
-                      handleYAxisColumnChange(index, 'fieldName', e)
-                    }
-                    onBlur={() => handleSave(chartData)}
-                    placeholder="Select"
-                  />
-                  <TextField
-                    name={`yAxisLabel-${index}`}
-                    label="Label"
-                    value={column.label}
-                    onChange={(e) => handleYAxisColumnChange(index, 'label', e)}
-                    onBlur={() => handleSave(chartData)}
-                  />
-                  <input
-                    name={`yAxisColor-${index}`}
-                    type="Color"
-                    value={column.color || '#000000'}
-                    onChange={(e: any) => {
-                      handleYAxisColumnChange(index, 'color', e.target.value);
-                    }}
-                    onBlur={() => handleSave(chartData)}
-                  />
-                  {isGroupedChart && index > 0 && (
-                    <Button onClick={() => removeYAxisColumn(index)}>
-                      Remove
-                    </Button>
-                  )}
-                </div>
-              ))}
+            <div className="flex flex-row flex-wrap justify-between  gap-4">
+              
+              <div className="flex flex-col gap-4 ">
+                {chartData?.options?.yAxisColumn?.map((column, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-wrap items-end gap-4 lg:flex-nowrap"
+                  >
+                    <Select
+                      name={`yAxisColumn-${index}`}
+                      options={resourceSchema
+                        ?.filter(
+                          (field) => field.format.toUpperCase() === 'INTEGER'
+                        )
+                        .map((field) => ({
+                          label: field.fieldName,
+                          value: field.id,
+                        }))}
+                      label="Y-axis Column"
+                      value={column.fieldName}
+                      onChange={(e) =>
+                        handleYAxisColumnChange(index, 'fieldName', e)
+                      }
+                      onBlur={() => handleSave(chartData)}
+                      placeholder="Select"
+                    />
+                    <TextField
+                      name={`yAxisLabel-${index}`}
+                      label="Label"
+                      value={column.label}
+                      onChange={(e) =>
+                        handleYAxisColumnChange(index, 'label', e)
+                      }
+                      onBlur={() => handleSave(chartData)}
+                    />
+                    <div className=" flex flex-col gap-1">
+                      <Text>Color</Text>
+                      <input
+                        name={`yAxisColor-${index}`}
+                        type="Color"
+                        className=" h-9"
+                        value={column.color || '#000000'}
+                        onChange={(e: any) => {
+                          handleYAxisColumnChange(
+                            index,
+                            'color',
+                            e.target.value
+                          );
+                        }}
+                        onBlur={() => handleSave(chartData)}
+                      />
+                    </div>
+                    {isGroupedChart && index > 0 && (
+                      <Button onClick={() => removeYAxisColumn(index)}>
+                        Remove
+                      </Button>
+                    )}
+                  </div>
+                ))}
+              </div>
               {isGroupedChart && (
-                <Button onClick={addYAxisColumn}>Add Y-axis Column</Button>
+                <Button className="h-fit w-fit mt-4" onClick={addYAxisColumn}>
+                  Add Y-axis Column
+                </Button>
               )}
             </div>
           )}
