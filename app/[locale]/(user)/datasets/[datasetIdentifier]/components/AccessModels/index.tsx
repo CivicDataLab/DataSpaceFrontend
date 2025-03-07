e3688c8e6131d89a97853bf67e4c6c562811c2fc
sanjaypinna <sanjaypinna265@gmail.com>
2025-03-07 11:53:29 +0530

    fix query and add types

--- app/[locale]/(user)/datasets/[datasetIdentifier]/components/AccessModels/index.tsx
+++ app/[locale]/(user)/datasets/[datasetIdentifier]/components/AccessModels/index.tsx
@@ -93,20 +93,16 @@ const generateTableData = (resources: any[]) => {
   }));
 };
 
-const accessModelResourcesQuery = graphql(`
+const accessModelResourcesQuery: any = graphql(`
   query accessModelResource($datasetId: UUID!) {
     accessModelResources(datasetId: $datasetId) {
-      modelResources {
+      resourceFields {
         resource {
           name
           description
           id
         }
-        fields {
-          fieldName
-          format
-          id
-        }
+        fields
       }
       id
       name
@@ -121,87 +117,91 @@ const accessModelResourcesQuery = graphql(`
 const AccessModels = () => {
   const params = useParams();
 
-  const { data, error, isLoading } = useQuery(
-    [`accessmodel_${params.datasetIdentifier}`],
-    () =>
-      GraphQL(
-        accessModelResourcesQuery,
-        {
-          // Entity Headers if present
-        },
-        {
-          datasetId: params.datasetIdentifier,
-        }
-      )
+  const getAccessModeldetails: {
+    data: any;
+    isError: boolean;
+    isLoading: boolean;
+  } = useQuery([`accessmodel_${params.datasetIdentifier}`], () =>
+    GraphQL(
+      accessModelResourcesQuery,
+      {
+        // Entity Headers if present
+      },
+      {
+        datasetId: params.datasetIdentifier,
+      }
+    )
   );
 
   return (
     <>
-      {isLoading ? (
+      {getAccessModeldetails.isLoading ? (
         <div className=" mt-8 flex justify-center">
           <Spinner />
         </div>
       ) : (
-        data?.accessModelResources.map((item: any, index: any) => (
-          <div
-            key={index}
-            className="my-4 flex flex-col gap-4 rounded-2 p-6 shadow-basicDeep"
-          >
-            <div className="mb-1 flex flex-wrap justify-between gap-1 lg:gap-0">
-              <div className="p2-4 lg:w-2/5">
-                <Text variant="headingMd">{item.name}</Text>
+        getAccessModeldetails.data?.accessModelResources.map(
+          (item: any, index: any) => (
+            <div
+              key={index}
+              className="my-4 flex flex-col gap-4 rounded-2 p-6 shadow-basicDeep"
+            >
+              <div className="mb-1 flex flex-wrap justify-between gap-1 lg:gap-0">
+                <div className="p2-4 lg:w-2/5">
+                  <Text variant="headingMd">{item.name}</Text>
+                </div>
+                <div className="lg:w-3/5 lg:pl-4">
+                  <Text>{item.description}</Text>
+                </div>
               </div>
-              <div className="lg:w-3/5 lg:pl-4">
-                <Text>{item.description}</Text>
+              <div className="align-center  flex flex-col justify-between gap-4 sm:flex-row lg:items-center">
+                <CustomTags type={item.type.split('.').pop().toLowerCase()} />
               </div>
-            </div>
-            <div className="align-center  flex flex-col justify-between gap-4 sm:flex-row lg:items-center">
-              <CustomTags type={item.type.split('.').pop().toLowerCase()} />
-            </div>
-            {item?.modelResources?.length > 0 && (
-              <div className="flex">
-                <Accordion type="single" collapsible className="w-full">
-                  <AccordionItem value="item-1" className=" border-none">
-                    <div className="flex flex-wrap items-center justify-between">
-                      <AccordionTrigger className="flex w-full flex-wrap items-center gap-2 hover:no-underline">
-                        <div className=" text-baseBlueSolid8  ">
-                          See Resources
-                        </div>
-                      </AccordionTrigger>
-                      <Button
-                        className="h-fit w-fit"
-                        kind="secondary"
-                        onClick={() => {
-                          item.modelResources.forEach((resource: any) => {
-                            // Construct the download URL for each resource
-                            const downloadUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/download/resource/${resource.resource.id}`;
-                            // Open the URL in a new tab
-                            window.open(downloadUrl, '_blank');
-                          });
+              {item?.modelResources?.length > 0 && (
+                <div className="flex">
+                  <Accordion type="single" collapsible className="w-full">
+                    <AccordionItem value="item-1" className=" border-none">
+                      <div className="flex flex-wrap items-center justify-between">
+                        <AccordionTrigger className="flex w-full flex-wrap items-center gap-2 hover:no-underline">
+                          <div className=" text-baseBlueSolid8  ">
+                            See Resources
+                          </div>
+                        </AccordionTrigger>
+                        <Button
+                          className="h-fit w-fit"
+                          kind="secondary"
+                          onClick={() => {
+                            item.modelResources.forEach((resource: any) => {
+                              // Construct the download URL for each resource
+                              const downloadUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/download/resource/${resource.resource.id}`;
+                              // Open the URL in a new tab
+                              window.open(downloadUrl, '_blank');
+                            });
+                          }}
+                        >
+                          Download All Resources
+                        </Button>
+                      </div>
+                      <AccordionContent
+                        className="flex w-full flex-col p-5"
+                        style={{
+                          backgroundColor: 'var( --base-pure-white)',
+                          outline: '1px solid var( --base-pure-white)',
                         }}
                       >
-                        Download All Resources
-                      </Button>
-                    </div>
-                    <AccordionContent
-                      className="flex w-full flex-col p-5"
-                      style={{
-                        backgroundColor: 'var( --base-pure-white)',
-                        outline: '1px solid var( --base-pure-white)',
-                      }}
-                    >
-                      <Table
-                        columns={generateColumnData()}
-                        rows={generateTableData(item.modelResources)}
-                        hideFooter
-                      />
-                    </AccordionContent>
-                  </AccordionItem>
-                </Accordion>
-              </div>
-            )}
-          </div>
-        ))
+                        <Table
+                          columns={generateColumnData()}
+                          rows={generateTableData(item.modelResources)}
+                          hideFooter
+                        />
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+                </div>
+              )}
+            </div>
+          )
+        )
       )}
     </>
   );
