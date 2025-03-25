a74ea0c31793953252cec0649466611c4563c34f
sanjaypinna <sanjaypinna265@gmail.com>
2025-03-25 15:52:46 +0530

    update breadcrumbs as per the design

--- components/BreadCrumbs/index.tsx
+++ components/BreadCrumbs/index.tsx
@@ -14,18 +14,18 @@ interface BreadCrumbsProps {
 
 const BreadCrumbs: React.FC<BreadCrumbsProps> = ({ data }) => {
   return (
-    <div className="bg-baseGraySlateSolid2 px-6 py-2 lg:px-10">
+    <div className=" bg-primaryBlue px-6 py-2 lg:px-10">
       <Breadcrumb className="mx-1">
-        <BreadcrumbList>
+        <BreadcrumbList className=' text-surfaceDefault'>
           {data.map((item, index) => (
             <React.Fragment key={index}>
               {index === data.length - 1 ? (
                 <BreadcrumbItem>
-                  <BreadcrumbPage>{item.label}</BreadcrumbPage>
+                  <BreadcrumbPage className=' text-surfaceDefault font-semi-bold'>{item.label}</BreadcrumbPage>
                 </BreadcrumbItem>
               ) : (
                 <BreadcrumbItem>
-                  <BreadcrumbLink href={item.href}>{item.label}</BreadcrumbLink>
+                  <BreadcrumbLink href={item.href} className=' text-surfaceDefault'>{item.label}</BreadcrumbLink>
                 </BreadcrumbItem>
               )}
               {index < data.length - 1 && <BreadcrumbSeparator />}
