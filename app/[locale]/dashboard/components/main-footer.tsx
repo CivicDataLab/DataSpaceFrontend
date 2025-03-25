4bdd057c22d93f3c755880851a3b3117afa30a76
sanjaypinna <sanjaypinna265@gmail.com>
2025-03-25 11:39:50 +0530

    update footer and add secondary color

--- app/[locale]/dashboard/components/main-footer.tsx
+++ app/[locale]/dashboard/components/main-footer.tsx
@@ -4,78 +4,62 @@ import Link from 'next/link';
 import { Icon, Text } from 'opub-ui';
 
 import { Icons } from '@/components/icons';
+import { IconBinary, IconBinaryTree } from '@tabler/icons-react';
 
 const MainFooter = () => {
   return (
     <>
-      <div
-        style={{
-          backgroundColor: 'var( --background-alpha-medium)',
-        }}
-      >
-        <div className="flex flex-wrap items-center justify-start gap-8 p-10 md:justify-center lg:justify-center lg:gap-32 ">
-          <div className="flex items-center gap-8 ">
-            <Link href="/">
-              <div className="flex items-center gap-2">
-                <Icon source={Icons.logo} size={24} color="success" />
+      <div className="bg-primaryBlue">
+        <div className="flex flex-col gap-8 p-6 lg:px-28 lg:py-10">
+          <div className="flex flex-wrap justify-between gap-8 ">
+            {' '}
+            <div className="flex items-center gap-2">
+              <Image src={'/logo.png'} width={38} height={38} alt="logo" />
+              <Text variant="headingXl" className="text-surfaceDefault" as="h1">
+                CivicDataSpace
+              </Text>
+            </div>
+            <div className=" flex flex-col  lg:items-end gap-2">
+              <div>
+                {' '}
                 <Text
-                  variant="headingLg"
-                  className="text-surfaceDefault"
-                  as="h1"
+                  color="highlight"
+                  className=" font-bold text-borderWarningSubdued"
                 >
-                  CivicDataSpace
+                  Follow Us
                 </Text>
               </div>
-            </Link>
-            <div>
-              <Link href={'https://civicdatalab.in'} target="_blank">
-                <Image src={'/cdl.svg'} width={96} alt={'cdl'} height={94} />
-              </Link>{' '}
-            </div>
-          </div>
-          <div className=" flex  gap-6">
-            <Link href={'#'} className=" text-baseIndigoSolid2">
-              About Us
-            </Link>
-            <Link href={'#'} className=" text-baseIndigoSolid2">
-              Sitemap
-            </Link>
-            <Link href={'#'} className=" text-baseIndigoSolid2">
-              Contact Us
-            </Link>
-          </div>
-          <div className=" flex flex-col gap-2">
-            <Text
-              color="highlight"
-              className=" font-bold text-borderWarningSubdued"
-            >
-              Follow Us
-            </Text>
-            <div className=" flex gap-3">
-              <div className="  h-10  w-10 rounded-5 bg-baseBlueSolid8 p-2">
-                <Icon source={Icons.twitter} size={24} color="onBgDefault" />
-              </div>
-              <div className="  h-10  w-10 rounded-5 bg-baseBlueSolid8 p-2">
-                <Icon source={Icons.linkedin} size={24} color="onBgDefault" />
-              </div>
-              <div className="  h-10  w-10 rounded-5 bg-baseBlueSolid8 p-2">
-                <Icon source={Icons.facebook} size={24} color="onBgDefault" />
-              </div>
-              <div className="  h-10  w-10 rounded-5 bg-baseBlueSolid8 p-2">
-                <Icon source={Icons.github} size={24} color="onBgDefault" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="  bg-baseBlueSolid6 p-2">
-        <div className=" m-auto flex  items-center gap-2 md:pl-4 lg:w-5/6 lg:pl-8">
-          <Icon source={Icons.info} size={24} />
 
-          <Text variant="bodyMd">
-            This Platform is designed, developed and hosted by CivicDataLab for
-            Data Exchange
-          </Text>
+              <div className=" flex gap-3">
+                <div className="  h-10  w-10 rounded-5 bg-secondaryOrange p-2">
+                  <Icon source={Icons.twitter} size={24} color="onBgDefault" />
+                </div>
+                <div className="  h-10  w-10 rounded-5 bg-secondaryOrange p-2">
+                  <Icon source={Icons.linkedin} size={24} color="onBgDefault" />
+                </div>
+                <div className="  h-10  w-10 rounded-5 bg-secondaryOrange p-2">
+                  <Icon source={Icons.facebook} size={24} color="onBgDefault" />
+                </div>
+                <div className="  h-10  w-10 rounded-5 bg-secondaryOrange p-2">
+                  <Icon source={Icons.github} size={24} color="onBgDefault" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className=" flex justify-between flex-wrap">
+            <div className=" flex  gap-6">
+              <Link href={'#'}>
+                <Text color="onBgDefault"> About Us</Text>
+              </Link>
+              <Link href={'#'}>
+                <Text color="onBgDefault"> Sitemap</Text>
+              </Link>
+              <Link href={'#'}>
+                <Text color="onBgDefault"> Contact Us</Text>
+              </Link>
+            </div>
+           
+          </div>
         </div>
       </div>
     </>
