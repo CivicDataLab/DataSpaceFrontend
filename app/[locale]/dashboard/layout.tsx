6313ac4aa1192dad7c5f10c7a44d03f65eddb49d
sanjaypinna <sanjaypinna265@gmail.com>
2025-03-28 13:39:33 +0530

    fix padding

--- app/[locale]/dashboard/layout.tsx
+++ app/[locale]/dashboard/layout.tsx
@@ -12,13 +12,13 @@ interface DashboardLayoutProps {
 export default function Layout({ children }: DashboardLayoutProps) {
   return (
     <div className="flex h-full grow flex-col">
-    <header className="relative z-2 p-6 lg:px-10 lg:py-6 bg-primaryBlue">
-      <MainNav />
-    </header>
-    <>{children}</>
-    <footer>
-      <MainFooter />
-    </footer>
-  </div>
+      <header className="relative z-2  bg-primaryBlue ">
+        <MainNav />
+      </header>
+      <>{children}</>
+      <footer>
+        <MainFooter />
+      </footer>
+    </div>
   );
 }
