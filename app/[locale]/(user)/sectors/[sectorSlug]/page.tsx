8939f0a3520f1556209d04fd149995386a9d76ee
sanjaypinna <sanjaypinna265@gmail.com>
2025-03-28 12:41:21 +0530

    update dataset listing

--- app/[locale]/(user)/sectors/[sectorSlug]/page.tsx
+++ app/[locale]/(user)/sectors/[sectorSlug]/page.tsx
@@ -7,14 +7,27 @@ import GraphqlPagination from '@/app/[locale]/dashboard/components/GraphqlPagina
 import { fetchDatasets } from '@/fetch';
 import { graphql } from '@/gql';
 import { useQuery } from '@tanstack/react-query';
-import { Pill, SearchInput, Select, Text } from 'opub-ui';
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  Divider,
+  Icon,
+  Pill,
+  SearchInput,
+  Select,
+  Text,
+  Tray,
+} from 'opub-ui';
 
 import { GraphQL } from '@/lib/api';
+import { cn } from '@/lib/utils';
 import BreadCrumbs from '@/components/BreadCrumbs';
 import { ErrorPage } from '@/components/error';
+import { Icons } from '@/components/icons';
 import { Loading } from '@/components/loading';
-import Card from '../../datasets/components/Card';
 import Filter from '../../datasets/components/FIlter/Filter';
+import Styles from '../../datasets/dataset.module.scss';
 
 const sectorQueryDoc: any = graphql(`
   query CategoryDetails($filters: SectorFilter) {
@@ -49,6 +62,8 @@ interface QueryParams {
   currentPage: number;
   filters: FilterOptions;
   query?: string;
+  sort?: string; // Adding sort to QueryParams
+  order?: string; // Adding sort to QueryParams
 }
 
 type Action =
@@ -57,13 +72,17 @@ type Action =
   | { type: 'SET_FILTERS'; payload: { category: string; values: string[] } }
   | { type: 'REMOVE_FILTER'; payload: { category: string; value: string } }
   | { type: 'SET_QUERY'; payload: string }
+  | { type: 'SET_SORT'; payload: string }
+  | { type: 'SET_ORDER'; payload: string }
   | { type: 'INITIALIZE'; payload: QueryParams };
 
 const initialState: QueryParams = {
-  pageSize: 5,
+  pageSize: 9,
   currentPage: 1,
   filters: {},
   query: '',
+  sort: 'recent', // Default sort is set to recent
+  order: '',
 };
 
 const queryReducer = (state: QueryParams, action: Action): QueryParams => {
@@ -94,6 +113,12 @@ const queryReducer = (state: QueryParams, action: Action): QueryParams => {
     case 'SET_QUERY': {
       return { ...state, query: action.payload };
     }
+    case 'SET_SORT': {
+      return { ...state, sort: action.payload };
+    }
+    case 'SET_ORDER': {
+      return { ...state, order: action.payload };
+    }
     case 'INITIALIZE': {
       return { ...state, ...action.payload };
     }
@@ -122,7 +147,7 @@ const useUrlParams = (
     });
 
     const initialParams: QueryParams = {
-      pageSize: sizeParam ? Number(sizeParam) : 5,
+      pageSize: sizeParam ? Number(sizeParam) : 9,
       currentPage: pageParam ? Number(pageParam) : 1,
       filters,
       query: urlParams.get('query') || '',
@@ -140,7 +165,13 @@ const useUrlParams = (
     const searchParam = queryParams.query
       ? `&query=${encodeURIComponent(queryParams.query)}`
       : '';
-    const variablesString = `?${filtersString}&size=${queryParams.pageSize}&page=${queryParams.currentPage}${searchParam}`;
+    const sortParam = queryParams.sort
+      ? `&sort=${encodeURIComponent(queryParams.sort)}`
+      : '';
+    const orderParam = queryParams.order
+      ? `&order=${encodeURIComponent(queryParams.order)}`
+      : '';
+    const variablesString = `?${filtersString}&size=${queryParams.pageSize}&page=${queryParams.currentPage}${searchParam}${sortParam}${orderParam}`;
     setVariables(variablesString);
 
     const currentUrl = new URL(window.location.href);
@@ -160,7 +191,16 @@ const useUrlParams = (
     } else {
       currentUrl.searchParams.delete('query');
     }
-
+    if (queryParams.sort) {
+      currentUrl.searchParams.set('sort', queryParams.sort);
+    } else {
+      currentUrl.searchParams.delete('sort');
+    }
+    if (queryParams.order) {
+      currentUrl.searchParams.set('order', queryParams.order);
+    } else {
+      currentUrl.searchParams.delete('order');
+    }
     router.push(currentUrl.toString());
   }, [queryParams, setVariables, router]);
 };
@@ -190,6 +230,7 @@ const SectorDetailsPage = ({ params }: { params: { categorySlug: any } }) => {
   const count = facets?.total ?? 0;
   const datasetDetails = facets?.results ?? [];
   const [queryParams, setQueryParams] = useReducer(queryReducer, initialState);
+  const [view, setView] = useState<'collapsed' | 'expanded'>('collapsed');
 
   useEffect(() => {
     if (variables) {
@@ -224,7 +265,13 @@ const SectorDetailsPage = ({ params }: { params: { categorySlug: any } }) => {
   const handleSearch = (searchTerm: string) => {
     setQueryParams({ type: 'SET_QUERY', payload: searchTerm });
   };
+  const handleSortChange = (sortOption: string) => {
+    setQueryParams({ type: 'SET_SORT', payload: sortOption });
+  };
 
+  const handleOrderChange = (sortOrder: string) => {
+    setQueryParams({ type: 'SET_ORDER', payload: sortOrder });
+  };
   const aggregations: Aggregations = facets?.aggregations || {};
 
   const filterOptions = Object.entries(aggregations).reduce(
@@ -286,96 +333,133 @@ const SectorDetailsPage = ({ params }: { params: { categorySlug: any } }) => {
               </Text>
             </div>
           </div>
-
-          <div>
-            <div className="mx-10 my-4 flex flex-wrap items-center justify-between gap-6 rounded-2 bg-baseBlueSolid4 px-4 py-2">
-              <div>
-                <Text>Showing 10 of 30 Datasets</Text>
-              </div>
-              <div className=" w-full max-w-[550px] md:block">
+          <div className="m-5 md:m-8 lg:m-10">
+            <div className="flex flex-wrap items-center justify-between gap-5 rounded-2 p-2 lg:flex-nowrap">
+              <div className=" w-full md:block">
                 <SearchInput
                   label="Search"
                   name="Search"
-                  // className={cn(Styles.Search)}
-                  placeholder="Search datasets"
-                  onSubmit={(value: any) => console.log(value)}
-                  onClear={(value: any) => console.log(value)}
+                  className={cn(Styles.Search)}
+                  placeholder="Start typing to search for any Dataset"
+                  onSubmit={(value) => handleSearch(value)}
+                  onClear={(value) => handleSearch(value)}
                 />
               </div>
-              <div className="flex flex-wrap items-center justify-between gap-4">
-                <div className="flex items-center gap-2">
-                  <Text
-                    variant="bodyLg"
-                    className="font-bold text-baseBlueSolid8"
-                  >
-                    Sort by:
-                  </Text>
-                  <Select
-                    label=""
-                    labelInline
-                    name="select"
-                    options={[
-                      {
-                        label: 'Newest',
-                        value: 'newestUpdate',
-                      },
-                      {
-                        label: 'Oldest',
-                        value: 'oldestUpdate',
-                      },
-                    ]}
-                  />
-                </div>
-                <div className="flex items-center gap-2">
-                  <Text
-                    variant="bodyLg"
-                    className="font-bold text-baseBlueSolid8"
-                  >
-                    Rows:
-                  </Text>
-                  <Select
-                    label=""
-                    labelInline
-                    name="select"
-                    options={[
-                      {
-                        label: '10',
-                        value: '10',
-                      },
-                      {
-                        label: '20',
-                        value: '20',
-                      },
-                    ]}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="row mx-10 mb-16 flex gap-5">
-            <div className="hidden min-w-64 max-w-64 lg:block">
-              <Filter
-                options={filterOptions}
-                setSelectedOptions={handleFilterChange}
-                selectedOptions={queryParams.filters}
-              />
-            </div>
-            <div className="flex h-full w-full flex-col px-2">
-              <div className="flex gap-2 border-b-2 border-solid border-baseGraySlateSolid4 pb-4">
-                {Object.entries(queryParams.filters).map(([category, values]) =>
-                  values.map((value) => (
-                    <Pill
-                      key={`${category}-${value}`}
-                      onRemove={() => handleRemoveFilter(category, value)}
+              <div className="flex flex-wrap justify-between gap-3 lg:flex-nowrap lg:justify-normal lg:gap-5">
+                <div className="hidden lg:flex items-center gap-2">
+                  <ButtonGroup noWrap spacing="tight">
+                    <Button
+                      kind={view === 'collapsed' ? 'secondary' : 'tertiary'}
+                      className=" h-fit w-fit"
+                      onClick={() => setView('collapsed')}
                     >
-                      {value}
-                    </Pill>
-                  ))
-                )}
+                      <Icon source={Icons.grid} />
+                    </Button>
+                    <Button
+                      onClick={() => setView('expanded')}
+                      kind={view === 'expanded' ? 'secondary' : 'tertiary'}
+                      className=" h-fit w-fit"
+                    >
+                      <Icon source={Icons.list} />
+                    </Button>
+                  </ButtonGroup>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Button
+                    onClick={() =>
+                      handleOrderChange(
+                        queryParams.order === 'asc' ? 'desc' : 'asc'
+                      )
+                    }
+                    kind="tertiary"
+                    className="h-fit w-fit"
+                    aria-label={`Sort ${queryParams.order === 'asc' ? 'descending' : 'ascending'}`}
+                  >
+                    <Icon
+                      source={Icons.sort}
+                      className={cn(
+                        queryParams.order === 'asc' && 'scale-x-[-1]'
+                      )}
+                    />
+                  </Button>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Select
+                    label=""
+                    labelInline
+                    name="select"
+                    onChange={handleSortChange}
+                    options={[
+                      {
+                        label: 'Recent',
+                        value: 'recent',
+                      },
+                      {
+                        label: 'Alphabetical',
+                        value: 'alphabetical',
+                      },
+                    ]}
+                  />
+                </div>
+
+                <Tray
+                  size="narrow"
+                  open={open}
+                  onOpenChange={setOpen}
+                  trigger={
+                    <Button
+                      kind="secondary"
+                      className="lg:hidden"
+                      onClick={() => setOpen(true)}
+                    >
+                      Filter
+                    </Button>
+                  }
+                >
+                  <Filter
+                    setOpen={setOpen}
+                    options={filterOptions}
+                    setSelectedOptions={handleFilterChange}
+                    selectedOptions={queryParams.filters}
+                  />
+                </Tray>
+              </div>
+            </div>
+            <div className="row mg:mt-8 mb-16 mt-5 flex gap-5 lg:mt-10">
+              <div className="hidden min-w-64 max-w-64 lg:block">
+                <Filter
+                  options={filterOptions}
+                  setSelectedOptions={handleFilterChange}
+                  selectedOptions={queryParams.filters}
+                />
               </div>
 
-              <div className="flex flex-col gap-6">
+              <div className="flex w-full flex-col gap-4 px-2">
+                {Object.values(queryParams.filters).filter(
+                  (value) => Array.isArray(value) && value.length !== 0
+                ).length > 1 && (
+                  <>
+                    <div className="flex gap-2">
+                      {Object.entries(queryParams.filters).map(
+                        ([category, values]) =>
+                          values
+                            .filter((value) => category !== 'sort')
+                            .map((value) => (
+                              <Pill
+                                key={`${category}-${value}`}
+                                onRemove={() =>
+                                  handleRemoveFilter(category, value)
+                                }
+                              >
+                                {value}
+                              </Pill>
+                            ))
+                      )}
+                    </div>
+                    <Divider className=" h-1 bg-surfaceDefault" />
+                  </>
+                )}
+
                 {facets && datasetDetails?.length > 0 && (
                   <GraphqlPagination
                     totalRows={count}
@@ -383,10 +467,55 @@ const SectorDetailsPage = ({ params }: { params: { categorySlug: any } }) => {
                     currentPage={queryParams.currentPage}
                     onPageChange={handlePageChange}
                     onPageSizeChange={handlePageSizeChange}
+                    view={view}
                   >
-                    {datasetDetails.map((item: any, index: any) => (
-                      <Card key={index} data={item} />
-                    ))}
+                    {datasetDetails.map((item: any, index: any) => {
+                      const commonProps = {
+                        title: item.title,
+                        description: item.description,
+                        metadataContent: [
+                          {
+                            icon: Icons.calendar,
+                            label: 'Date',
+                            value: '19 July 2024',
+                          },
+                          {
+                            icon: Icons.download,
+                            label: 'Download',
+                            value: item.download_count.toString(),
+                          },
+                          {
+                            icon: Icons.globe,
+                            label: 'Geography',
+                            value: 'India',
+                          },
+                        ],
+                        tag: item.tags,
+                        formats: item.formats,
+                        footerContent: [
+                          {
+                            icon: '',
+                            label: 'Sectors',
+                          },
+                          {
+                            icon: '',
+                            label: 'Published by',
+                          },
+                        ],
+                      };
+
+                      return (
+                        <Card
+                          {...commonProps}
+                          key={item.id}
+                          variation={
+                            view === 'expanded' ? 'expanded' : 'collapsed'
+                          }
+                          iconColor="warning"
+                          href={`/datasets/${item.id}`}
+                        />
+                      );
+                    })}
                   </GraphqlPagination>
                 )}
               </div>
