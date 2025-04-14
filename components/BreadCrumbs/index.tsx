36ec55e14d0127fad4fafff7f5f0ae34f347a2a9
sanjaypinna <sanjaypinna265@gmail.com>
2025-04-14 13:16:19 +0530

    update the color of breadcrumbs

--- components/BreadCrumbs/index.tsx
+++ components/BreadCrumbs/index.tsx
@@ -14,18 +14,18 @@ interface BreadCrumbsProps {
 
 const BreadCrumbs: React.FC<BreadCrumbsProps> = ({ data }) => {
   return (
-    <div className=" bg-primaryBlue px-6 py-2 lg:px-10">
+    <div className=" bg-secondaryOrange px-6 py-2 lg:px-10">
       <Breadcrumb className="mx-1">
-        <BreadcrumbList className=' text-surfaceDefault'>
+        <BreadcrumbList className=''>
           {data.map((item, index) => (
             <React.Fragment key={index}>
               {index === data.length - 1 ? (
                 <BreadcrumbItem>
-                  <BreadcrumbPage className=' text-surfaceDefault font-semi-bold'>{item.label}</BreadcrumbPage>
+                  <BreadcrumbPage className='font-bold'>{item.label}</BreadcrumbPage>
                 </BreadcrumbItem>
               ) : (
                 <BreadcrumbItem>
-                  <BreadcrumbLink href={item.href} className=' text-surfaceDefault'>{item.label}</BreadcrumbLink>
+                  <BreadcrumbLink href={item.href} className=' font-bold '>{item.label}</BreadcrumbLink>
                 </BreadcrumbItem>
               )}
               {index < data.length - 1 && <BreadcrumbSeparator />}
