90c6873ec78bc6e190921e1130d87f5447278144
sanjaypinna <sanjaypinna265@gmail.com>
2025-04-14 20:29:19 +0530

    update usecase listing

--- app/[locale]/(user)/usecases/page.tsx
+++ app/[locale]/(user)/usecases/page.tsx
@@ -3,14 +3,18 @@
 import Image from 'next/image';
 import { graphql } from '@/gql';
 import { useQuery } from '@tanstack/react-query';
-import { Card, Text } from 'opub-ui';
+import { Card, Spinner, Text } from 'opub-ui';
 
 import { GraphQL } from '@/lib/api';
+import { cn, formatDate } from '@/lib/utils';
 import BreadCrumbs from '@/components/BreadCrumbs';
+import { Icons } from '@/components/icons';
+import { Loading } from '@/components/loading';
+import Styles from '../page.module.scss';
 
 const useCasesListQueryDoc: any = graphql(`
-  query UseCasesList {
-    useCases {
+  query UseCasesList($filters: UseCaseFilter) {
+    useCases(filters: $filters) {
       id
       title
       summary
@@ -19,7 +23,27 @@ const useCasesListQueryDoc: any = graphql(`
       logo {
         path
       }
+      metadata {
+        metadataItem {
+          id
+          label
+          dataType
+        }
+        id
+        value
+      }
+      publishers {
+        logo {
+          path
+        }
+        name
+      }
+      sectors {
+        id
+        name
+      }
       created
+      modified
       website
       contactEmail
     }
@@ -35,13 +59,14 @@ const UseCasesListingPage = () => {
   } = useQuery([`useCases_list_page`], () =>
     GraphQL(
       useCasesListQueryDoc,
+      {},
       {
-        // Entity Headers if present
-      },
-      []
+        filters: { status: 'PUBLISHED' },
+      }
     )
   );
-
+  console.log(getUseCasesList.data);
+  
 
   return (
     <main className="bg-baseGraySlateSolid2">
@@ -73,26 +98,61 @@ const UseCasesListingPage = () => {
             width={600}
             height={316}
             alt={'Usecase Illustration'}
-            className=" m-auto"
+            className=" m-auto h-auto w-full"
           />
         </div>
       </div>
-      <div className="container p-10 py-20">
+      <div className="container p-6 py-20 lg:p-10">
         <div>
           <Text variant="heading3xl">Explore Use Cases</Text>
         </div>
-        <div className=" py-10">
-          {getUseCasesList &&
-            getUseCasesList?.data?.useCases.length > 0 &&
-            getUseCasesList?.data?.useCases.map((item: any, index: any) => (
-              <Card
-                title={item.title}
-                key={index}
-                variation={'collapsed'}
-                iconColor={'default'}
-              />
-            ))}
-        </div>
+        {getUseCasesList.isLoading ? (
+          <div className=" mt-8 flex justify-center">
+            <Spinner />
+          </div>
+        ) : (
+          <div
+            className={cn(
+              'grid w-full grid-cols-1 gap-6 pt-10 md:grid-cols-2 lg:grid-cols-3',
+              Styles.Card
+            )}
+          >
+            {getUseCasesList &&
+              getUseCasesList?.data?.useCases.length > 0 &&
+              getUseCasesList?.data?.useCases.map((item: any, index: any) => (
+                <Card
+                  title={item.title}
+                  key={index}
+                  href={`/usecases/${item.id}`}
+                  metadataContent={[
+                    {
+                      icon: Icons.calendar,
+                      label: 'Date',
+                      value: formatDate(item.modified),
+                    },
+                    {
+                      icon: Icons.globe,
+                      label: 'Geography',
+                      value: item.metadata?.find(
+                        (meta: any) => meta.metadataItem?.label === 'Geography'
+                      )?.value,
+                    },
+                  ]}
+                  footerContent={[
+                    {
+                      icon: `/Sectors/${item.sectors[0].name}.svg`,
+                      label: 'Sectors',
+                    },
+                    { icon: '/fallback.svg', label: 'Published by' },
+                  ]}
+                  imageUrl={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${item.logo?.path.replace('/code/files/', '')}`}
+                  description={item.summary}
+                  iconColor="warning"
+                  variation={'collapsed'}
+                />
+              ))}
+          </div>
+        )}
       </div>
     </main>
   );
