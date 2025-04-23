63b3bfb857f612642a1918b1cc87021d4f63c99b
sanjaypinna <sanjaypinna265@gmail.com>
2025-04-23 15:01:43 +0530

    Make header and breadcrumbs sticky

--- components/BreadCrumbs/index.tsx
+++ components/BreadCrumbs/index.tsx
@@ -14,14 +14,14 @@ interface BreadCrumbsProps {
 
 const BreadCrumbs: React.FC<BreadCrumbsProps> = ({ data }) => {
   return (
-    <div className=" bg-secondaryOrange px-6 py-2 lg:px-10">
+    <div className="sticky top-[70px] lg:top-[85px] z-1 bg-secondaryOrange px-6 py-2 lg:px-10 border-b">
       <Breadcrumb className="mx-1">
         <BreadcrumbList className=''>
           {data.map((item, index) => (
             <React.Fragment key={index}>
               {index === data.length - 1 ? (
                 <BreadcrumbItem>
-                  <BreadcrumbPage className='font-bold'>{item.label}</BreadcrumbPage>
+                  <BreadcrumbPage className='font-bold' >{item.label}</BreadcrumbPage>
                 </BreadcrumbItem>
               ) : (
                 <BreadcrumbItem>
