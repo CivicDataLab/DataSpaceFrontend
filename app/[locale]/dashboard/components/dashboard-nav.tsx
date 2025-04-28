04ac69ee6e57110b9b97075dd93a61ffa2fe6b52
sanjaypinna <sanjaypinna265@gmail.com>
2025-04-28 11:28:46 +0530

    fix index

--- app/[locale]/dashboard/components/dashboard-nav.tsx
+++ app/[locale]/dashboard/components/dashboard-nav.tsx
@@ -48,7 +48,7 @@ export function DashboardNav({
     <aside
       className={cn(
         'overflow-hidden bg-surfaceDefault ',
-        ' z-1 mb-10 hidden shrink-0 basis-[260px]  rounded-2 border-2  border-solid  border-baseGraySlateSolid6 shadow-insetBasic md:block',
+        ' mb-10 hidden shrink-0 basis-[260px]  rounded-2 border-2  border-solid  border-baseGraySlateSolid6 shadow-insetBasic md:block',
         isCollapsed && 'basis-[60px]',
         styles.Collapse
       )}
