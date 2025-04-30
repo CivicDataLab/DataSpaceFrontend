f5c2f176344f446a348bac7a9a33f1c53c90a5cc
sanjaypinna <sanjaypinna265@gmail.com>
2025-04-30 15:12:58 +0530

    refactor store to implement DashboardStore for improved state management of entity and user details

--- config/store.ts
+++ config/store.ts
@@ -11,4 +11,22 @@ export const useIsNavigating = create<NavigationProps>((set) => ({
   setIsNavigation: (isNavigating) => set({ isNavigating }),
 }));
 
+interface DashboardStore {
+  entityDetails: any;
+  userDetails: any;
+  setEntityDetails: (data: any) => void;
+  setUserDetails: (data: any) => void;
+  allEntityDetails: any;
+  setAllEntityDetails: (data: any) => void;
+}
+
+export const useDashboardStore = create<DashboardStore>((set) => ({
+  entityDetails: null,
+  userDetails: null,
+  setEntityDetails: (data) => set({ entityDetails: data }),
+  setUserDetails: (data) => set({ userDetails: data }),
+  allEntityDetails: null,
+  setAllEntityDetails: (data) => set({ allEntityDetails: data }),
+}));
+
 export { shallow };
