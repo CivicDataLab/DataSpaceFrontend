dcd0a4857c98be5aad5abf704cc6cba6939d2f7b
sanjaypinna <sanjaypinna265@gmail.com>
2025-05-06 08:50:06 +0530

    move queries and mutations to query.ts

--- app/[locale]/dashboard/[entityType]/[entitySlug]/usecases/edit/[id]/contributors/page.tsx
+++ app/[locale]/dashboard/[entityType]/[entitySlug]/usecases/edit/[id]/contributors/page.tsx
@@ -13,176 +13,8 @@ import { Loading } from '@/components/loading';
 import { useEditStatus } from '../../context';
 import CustomCombobox from './CustomCombobox';
 import EntitySection from './EntitySelection';
+import { FetchUsers, FetchUsecaseInfo, AddContributors, RemoveContributor, AddSupporters, RemoveSupporters, AddPartners, RemovePartners } from './query';
 
-const FetchUsers: any = graphql(`
-  query searchUsers($limit: Int!, $searchTerm: String!) {
-    searchUsers(limit: $limit, searchTerm: $searchTerm) {
-      id
-      fullName
-      username
-    }
-  }
-`);
-
-const FetchUsecaseInfo: any = graphql(`
-  query useCaseinfo($filters: UseCaseFilter) {
-    useCases(filters: $filters) {
-      id
-      title
-      contributors {
-        id
-        fullName
-        username
-      }
-      supportingOrganizations {
-        id
-        name
-        logo {
-          url
-          name
-        }
-      }
-      partnerOrganizations{
-        id
-        name
-        logo{
-          url
-          name
-        }
-      }  
-    }
-  }
-`);
-
-const AddContributors: any = graphql(`
-  mutation addContributorToUseCase($useCaseId: String!, $userId: ID!) {
-    addContributorToUseCase(useCaseId: $useCaseId, userId: $userId) {
-      __typename
-      ... on TypeUseCase {
-        id
-        title
-        contributors {
-          id
-          fullName
-          username
-        }
-      }
-    }
-  }
-`);
-
-const RemoveContributor: any = graphql(`
-  mutation removeContributorFromUseCase($useCaseId: String!, $userId: ID!) {
-    removeContributorFromUseCase(useCaseId: $useCaseId, userId: $userId) {
-      __typename
-      ... on TypeUseCase {
-        id
-        title
-        contributors {
-          id
-          fullName
-          username
-        }
-      }
-    }
-  }
-`);
-
-const AddSupporters: any = graphql(`
-  mutation addSupportingOrganizationToUseCase(
-    $useCaseId: String!
-    $organizationId: ID!
-  ) {
-    addSupportingOrganizationToUseCase(
-      useCaseId: $useCaseId
-      organizationId: $organizationId
-    ) {
-      __typename
-      ... on TypeUseCaseOrganizationRelationship {
-        organization {
-          id
-          name
-          logo {
-            url
-            name
-          }
-        }
-      }
-    }
-  }
-`);
-
-const RemoveSupporters: any = graphql(`
-  mutation removeSupportingOrganizationFromUseCase(
-    $useCaseId: String!
-    $organizationId: ID!
-  ) {
-    removeSupportingOrganizationFromUseCase(
-      useCaseId: $useCaseId
-      organizationId: $organizationId
-    ) {
-      __typename
-      ... on TypeUseCaseOrganizationRelationship {
-        organization {
-          id
-          name
-          logo {
-            url
-            name
-          }
-        }
-      }
-    }
-  }
-`);
-
-const AddPartners: any = graphql(`
-  mutation addPartnerOrganizationToUseCase(
-    $useCaseId: String!
-    $organizationId: ID!
-  ) {
-    addPartnerOrganizationToUseCase(
-      useCaseId: $useCaseId
-      organizationId: $organizationId
-    ) {
-      __typename
-      ... on TypeUseCaseOrganizationRelationship {
-        organization {
-          id
-          name
-          logo {
-            url
-            name
-          }
-        }
-      }
-    }
-  }
-`);
-
-const RemovePartners: any = graphql(`
-  mutation removePartnerOrganizationFromUseCase(
-    $useCaseId: String!
-    $organizationId: ID!
-  ) {
-    removePartnerOrganizationFromUseCase(
-      useCaseId: $useCaseId
-      organizationId: $organizationId
-    ) {
-      __typename
-      ... on TypeUseCaseOrganizationRelationship {
-        organization {
-          id
-          name
-          logo {
-            url
-            name
-          }
-        }
-      }
-    }
-  }
-`);
 
 const Details = () => {
   const params = useParams<{ id: string }>();
@@ -346,24 +178,19 @@ const Details = () => {
 
   const { setStatus } = useEditStatus();
 
-  useEffect(() => {
-    setStatus(
-      addContributorLoading ||
-        removeContributorLoading ||
-        addSupporterLoading ||
-        removeSupporterLoading ||
-        addPartnerLoading 
-        ? 'loading'
-        : 'success'
-    ); // update based on mutation state
-  }, [
+
+  const loadingStates = [
     addContributorLoading,
     removeContributorLoading,
     addSupporterLoading,
     removeSupporterLoading,
     addPartnerLoading,
-  ]);
+    removePartnerLoading,
+  ];
 
+  useEffect(() => {
+    setStatus(loadingStates.some(Boolean) ? 'loading' : 'success');
+  }, loadingStates);
 
   return (
     <div>
