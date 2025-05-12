16c6c1e4238dc8eafcc19ece86247422f8ff9e14
sanjaypinna <sanjaypinna265@gmail.com>
2025-05-12 16:48:59 +0530

    refactor(Publish): enhance contributor and organization details display with images

--- app/[locale]/dashboard/[entityType]/[entitySlug]/usecases/edit/[id]/publish/page.tsx
+++ app/[locale]/dashboard/[entityType]/[entitySlug]/usecases/edit/[id]/publish/page.tsx
@@ -68,6 +68,9 @@ const UseCaseDetails: any = graphql(`
         id
         fullName
         username
+        profilePicture {
+          url
+        }
       }
       supportingOrganizations {
         id
@@ -188,11 +191,40 @@ const Publish = () => {
     })) || []),
   ];
 
-
   const ContributorDetails = [
-    { label: 'Contributors', value: UseCaseData.data?.useCases[0]?.contributors.length>0 ? UseCaseData.data?.useCases[0]?.contributors.map((item: any) => item.fullName).join(', '):'No Contributors' },
-    { label: 'Supporters', value: UseCaseData.data?.useCases[0]?.supportingOrganizations.length>0 ? UseCaseData.data?.useCases[0]?.supportingOrganizations.map((item: any) => item.name).join(', '):'No Supporting Organizations' },
-    { label: 'Partners', value: UseCaseData.data?.useCases[0]?.partnerOrganizations.length>0 ? UseCaseData.data?.useCases[0]?.partnerOrganizations.map((item: any) => item.name).join(', '):'No Partner Organizations' },
+    {
+      label: 'Contributors',
+      value:
+        UseCaseData.data?.useCases[0]?.contributors.length > 0
+          ? UseCaseData.data?.useCases[0]?.contributors
+              .map((item: any) => item.fullName)
+              .join(', ')
+          : 'No Contributors',
+      image: UseCaseData.data?.useCases[0]?.contributors,
+    },
+  ];
+
+  const OrgDetails = [
+    {
+      label: 'Supporters',
+      value:
+        UseCaseData.data?.useCases[0]?.supportingOrganizations.length > 0
+          ? UseCaseData.data?.useCases[0]?.supportingOrganizations
+              .map((item: any) => item.name)
+              .join(', ')
+          : 'No Supporting Organizations',
+      image: UseCaseData.data?.useCases[0]?.supportingOrganizations,
+    },
+    {
+      label: 'Partners',
+      value:
+        UseCaseData.data?.useCases[0]?.partnerOrganizations.length > 0
+          ? UseCaseData.data?.useCases[0]?.partnerOrganizations
+              .map((item: any) => item.name)
+              .join(', ')
+          : 'No Partner Organizations',
+      image: UseCaseData.data?.useCases[0]?.partnerOrganizations,
+    },
   ];
 
   const generateTableData = (list: Array<any>) => {
@@ -304,26 +336,84 @@ const Publish = () => {
                           </div>
                         ) : (
                           <div className="flex flex-col gap-4 px-8 py-4">
-                            {                         
-                              ContributorDetails.map(
-                                (item: any, index: number) => (
-                                  <div
-                                    className="flex flex-wrap gap-2"
-                                    key={index}
-                                  >
-                                    <div className="md:w-1/6 lg:w-1/6">
-                                        <Text variant="bodyMd">
-                                          {item.label}:
+                            {ContributorDetails.map(
+                              (item: any, index: number) => (
+                                <div
+                                  className="flex flex-col gap-3"
+                                  key={index}
+                                >
+                                  <div>
+                                    <Text variant="bodyMd">{item.label}:</Text>
+                                  </div>
+                                  <div className="flex flex-wrap gap-2">
+                                    {item.image.map(
+                                      (data: any, index: number) => (
+                                        <div
+                                          key={index}
+                                          className="flex flex-col items-center gap-4"
+                                        >
+                                          <Image
+                                            src={
+                                              data?.profilePicture?.url
+                                                ? `${process.env.NEXT_PUBLIC_BACKEND_URL}/${data.profilePicture.url}`
+                                                : '/profile.png'
+                                            }
+                                            alt={item.label}
+                                            width={80}
+                                            height={80}
+                                            className="rounded-full object-cover"
+                                          />
+
+                                          <Text
+                                            variant="bodyMd"
+                                            className=" w-36 rounded-1 bg-greyExtralight text-center"
+                                          >
+                                            {data.fullName}
+                                          </Text>
+                                        </div>
+                                      )
+                                    )}
+                                  </div>
+                                </div>
+                              )
+                            )}
+                            {OrgDetails.map((item: any, index: number) => (
+                              <div className="flex flex-col gap-3" key={index}>
+                                <div>
+                                  <Text variant="bodyMd">{item.label}:</Text>
+                                </div>
+                                <div className="flex flex-wrap gap-6">
+                                  {item.image.map(
+                                    (data: any, index: number) => (
+                                      <div
+                                        key={index}
+                                        className="flex flex-col items-center gap-4"
+                                      >
+                                        <div className="rounded-4 bg-surfaceDefault p-4 shadow-basicMd">
+                                          <Image
+                                            src={
+                                              data?.logo?.url
+                                                ? `${process.env.NEXT_PUBLIC_BACKEND_URL}/${data.logo.url}`
+                                                : '/org.png'
+                                            }
+                                            alt={item.label}
+                                            width={140}
+                                            height={100}
+                                            className="object-contain"
+                                          />
+                                        </div>
+                                        <Text
+                                          variant="bodyMd"
+                                          className=" w-36 rounded-1 bg-greyExtralight text-center"
+                                        >
+                                          {data.name}
                                         </Text>
                                       </div>
-                                    <div>
-                                      <Text variant="bodyMd">
-                                        {item.value}
-                                      </Text>
-                                    </div>
-                                  </div>
-                                )
-                              )}
+                                    )
+                                  )}
+                                </div>
+                              </div>
+                            ))}
                           </div>
                         )}
                       </div>
