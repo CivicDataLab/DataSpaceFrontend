e0b81ffa8e871360f33ca8135682d8a190ab2994
sanjaypinna <sanjaypinna265@gmail.com>
2025-05-23 10:07:42 +0530

    add usecases

--- app/[locale]/(user)/page.tsx
+++ app/[locale]/(user)/page.tsx
@@ -1,13 +1,15 @@
 import { Content } from './components/Content';
 import Datasets from './components/Datasets';
 import Sectors from './components/Sectors';
+import UseCases from './components/UseCases';
 
 export default async function Home() {
   return (
     <div className="bg-surfaceDefault">
       <Content />
+      <UseCases />
+      <Sectors />
       <Datasets />
-      <Sectors/>
     </div>
   );
 }
