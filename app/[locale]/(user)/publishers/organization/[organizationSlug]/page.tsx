7b49b7afe65069369fd9123af848fb1d33082d4e
sanjaypinna <sanjaypinna265@gmail.com>
2025-05-25 17:30:16 +0530

    Add OrgPage component

---
+++ app/[locale]/(user)/publishers/organization/[organizationSlug]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Loading } from '@/components/loading';
+
+const OrgPage = () => {
+  return (
+    <div>
+      <Loading />
+    </div>
+  );
+};
+
+export default OrgPage;
