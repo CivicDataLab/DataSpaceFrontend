a2cdeeb68fc8b806f35f12899536141ad59ded14
sanjaypinna <sanjaypinna265@gmail.com>
2025-06-06 06:49:49 +0530

    redirect user to datasets page

--- app/[locale]/dashboard/[entityType]/[entitySlug]/page.tsx
+++ app/[locale]/dashboard/[entityType]/[entitySlug]/page.tsx
@@ -1,9 +1,18 @@
-import { InProgress } from '@/components/in-progress';
+'use client';
 
-export default async function Page({ params }: { params: { id: string } }) {
-  return (
-    <>
-      <InProgress />
-    </>
-  );
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function Page({
+  params,
+}: {
+  params: { entityType: string; entitySlug: string };
+}) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push(`/dashboard/${params.entityType}/${params.entitySlug}/dataset?tab=drafts`);
+  }, [params, router]);
+
+  return null; // prevent rendering anything before redirect
 }
