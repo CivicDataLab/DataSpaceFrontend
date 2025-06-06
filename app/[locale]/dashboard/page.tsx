d02b0cf1a53881cb2a1a313cac4707efb633d3b6
sanjaypinna <sanjaypinna265@gmail.com>
2025-06-06 16:40:58 +0530

    fix height in user dashboard

--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -3,10 +3,10 @@
 import Link from 'next/link';
 import { Icon, Text } from 'opub-ui';
 
-import { useDashboardStore } from '@/config/store';
 import BreadCrumbs from '@/components/BreadCrumbs';
 import { Icons } from '@/components/icons';
 import { Loading } from '@/components/loading';
+import { useDashboardStore } from '@/config/store';
 
 const UserDashboard = () => {
   const { userDetails, allEntityDetails } = useDashboardStore();
@@ -23,7 +23,6 @@ const UserDashboard = () => {
     },
   ];
 
-  
   return (
     <>
       <BreadCrumbs
@@ -39,21 +38,23 @@ const UserDashboard = () => {
       {!userDetails?.me ? (
         <Loading />
       ) : (
-        <div className="container mb-40 ">
+        <div className="flex-1 container mb-40 ">
           <div className=" flex flex-col gap-6 py-10">
             <Text variant="headingXl"> User Dashboard</Text>
           </div>
-          <div className="flex flex-wrap items-center gap-6 md:flex-nowrap lg:flex-nowrap">
-            {list.map((item, index) => (
-              <Link
-                key={index}
-                href={item.path}
-                className=" flex max-h-56 min-h-56 w-full flex-col  items-center justify-center gap-3 rounded-4 bg-greyExtralight p-4 "
-              >
-                <Icon source={item.icon} size={60} color="highlight" />
-                <Text variant="headingLg">{item.label}</Text>
-              </Link>
-            ))}
+          <div className="flex-1 ">
+            <div className="flex flex-wrap items-center gap-6 md:flex-nowrap lg:flex-nowrap">
+              {list.map((item, index) => (
+                <Link
+                  key={index}
+                  href={item.path}
+                  className=" flex max-h-56 min-h-56 w-full flex-col  items-center justify-center gap-3 rounded-4 bg-greyExtralight p-4 "
+                >
+                  <Icon source={item.icon} size={60} color="highlight" />
+                  <Text variant="headingLg">{item.label}</Text>
+                </Link>
+              ))}
+            </div>
           </div>
         </div>
       )}
