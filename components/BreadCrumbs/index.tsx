4edcefb1b4133405b2f623adb90ebb58239d7d5a
sanjaypinna <sanjaypinna265@gmail.com>
2025-06-30 16:15:46 +0530

    fix: adjust top positioning and padding in BreadCrumbs component

--- components/BreadCrumbs/index.tsx
+++ components/BreadCrumbs/index.tsx
@@ -14,7 +14,7 @@ interface BreadCrumbsProps {
 
 const BreadCrumbs: React.FC<BreadCrumbsProps> = ({ data }) => {
   return (
-    <div className="border-b sticky top-[70px] z-1 bg-secondaryOrange px-6 py-2 lg:top-[106px] lg:px-10">
+    <div className="border-b sticky top-[70px] z-1 bg-secondaryOrange px-6 py-2 lg:top-[86px] ">
       <Breadcrumb className="mx-1">
         <BreadcrumbList className="">
           {data.map((item, index) => (
