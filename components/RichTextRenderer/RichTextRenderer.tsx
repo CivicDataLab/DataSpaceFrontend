141433c7f287b830653d5d815f2a01fff80089c6
dc <eopoxf@gmail.com>
2025-12-08 12:24:35 +0530

    udpate renderer import

--- components/RichTextRenderer/RichTextRenderer.tsx
+++ components/RichTextRenderer/RichTextRenderer.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import 'react-quill/dist/quill.snow.css';
+import 'react-quill-new/dist/quill.snow.css';
 
 interface RichTextRendererProps {
   content: string;
