1c3fa1775e68882dd8907f658bf9186433736ed2
UdayRajSahai2 <udayrajsahai22@gmail.com>
2026-03-27 11:44:30 +0530

    Fixed conflict

--- app/[locale]/dashboard/[entityType]/[entitySlug]/aimodels/edit/[id]/details/page.tsx
+++ app/[locale]/dashboard/[entityType]/[entitySlug]/aimodels/edit/[id]/details/page.tsx
@@ -305,7 +305,7 @@ export default function AIModelDetailsPage() {
     }
 
     if (!isValidHttpUrl(trimmedWebsite)) {
-      toast('Please enter a valid URL that includes http or https.', {
+      toast('Please enter a valid URL that includes https.', {
         id: AI_MODEL_VALIDATION_TOAST_ID,
       });
       return;
